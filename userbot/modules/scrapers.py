580483f84f1bc48b1b2f393ab1d2bb89f3baa279
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-04-08 15:53:19 +0700

    scrapers: currency: switch to ratesapi.io

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -185,26 +185,31 @@ async def img_sampler(event):
     await event.delete()
 
 
-@register(outgoing=True, pattern=r"^\.currency ([\d\.]+) ([a-zA-Z]+) ([a-zA-Z]+)")
+@register(outgoing=True, pattern=r"^\.currency (.*)")
 async def moni(event):
-    c_from_val = float(event.pattern_match.group(1))
-    c_from = (event.pattern_match.group(2)).upper()
-    c_to = (event.pattern_match.group(3)).upper()
-    try:
-        response = get(
-            "https://api.ratesapi.io/api/latest",
-            params={"base": c_from, "symbols": c_to},
-        ).json()
-    except Exception:
-        await event.edit("**Error: API is down.**")
-        return
-    if "error" in response:
-        await event.edit(
-            "`Sepertinya ini adalah mata uang asing, yang tidak dapat saya konversi sekarang.`"
-        )
-        return
-    c_to_val = round(c_from_val * response["rates"][c_to], 2)
-    await event.edit(f"**{c_from_val} {c_from} = {c_to_val} {c_to}**")
+    input_str = event.pattern_match.group(1)
+    input_sgra = input_str.split(" ")
+    if len(input_sgra) == 3:
+        try:
+            number = float(input_sgra[0])
+            currency_from = input_sgra[1].upper()
+            currency_to = input_sgra[2].upper()
+            request_url = f"https://api.ratesapi.io/api/latest?base={currency_from}"
+            current_response = get(request_url).json()
+            if currency_to in current_response["rates"]:
+                current_rate = float(current_response["rates"][currency_to])
+                rebmun = round(number * current_rate, 2)
+                await event.edit(
+                    "{} {} = {} {}".format(number, currency_from, rebmun, currency_to)
+                )
+            else:
+                await event.edit(
+                    "`Sepertinya ini adalah mata uang asing, yang tidak dapat saya konversi sekarang.`"
+                )
+        except Exception as e:
+            await event.edit(str(e))
+    else:
+        return await event.edit("`Invalid syntax.`")
 
 
 @register(outgoing=True, pattern=r"^\.google (.*)")
