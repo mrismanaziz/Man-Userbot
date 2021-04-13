a3cf8537117c9475c8083f46e5937fa58b6e413b
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-04-13 13:41:22 +0700

    progress: handle MessageNotModifiedError

--- userbot/utils/progress.py
+++ userbot/utils/progress.py
@@ -18,39 +18,48 @@
 import time
 import math
 
+from telethon.errors.rpcerrorlist import MessageNotModifiedError
+
 from .tools import humanbytes, time_formatter
 from .exceptions import CancelProcess
 
 
 async def progress(
-    current, total, gdrive, start, prog_type, file_name=None, is_cancelled=False
+    current, total, event, start, prog_type,
+    file_name=None, is_cancelled=False
 ):
     now = time.time()
     diff = now - start
     if is_cancelled is True:
         raise CancelProcess
 
-    if round(diff % 10.00) == 0 or current == total:
+    if round(diff % 15.00) == 0 or current == total:
         percentage = current * 100 / total
         speed = current / diff
         elapsed_time = round(diff)
         eta = round((total - current) / speed)
-        if "upload" in prog_type.lower():
-            status = "Uploading"
-        elif "download" in prog_type.lower():
-            status = "Downloading"
+        if 'upload' in prog_type.lower():
+            status = 'Uploading'
+        elif 'download' in prog_type.lower():
+            status = 'Downloading'
         else:
-            status = "Unknown"
-        progress_str = "[{0}{1}] `{2}%`".format(
-            "".join("█" for i in range(math.floor(percentage / 10))),
-            "".join("░" for i in range(10 - math.floor(percentage / 10))),
-            round(percentage, 2),
-        )
+            status = 'Unknown'
+        progress_str = "`{0}` | [{1}{2}] `{3}%`".format(
+            status,
+            ''.join(["●" for i in range(
+                    math.floor(percentage / 10))]),
+            ''.join(["○" for i in range(
+                    10 - math.floor(percentage / 10))]),
+            round(percentage, 2))
         tmp = (
-            f"{progress_str} - {status}\n"
-            f"`Size:` {humanbytes(current)} of {humanbytes(total)}\n"
-            f"`Speed:` {humanbytes(speed)}\n"
-            f"`ETA:` {time_formatter(eta)}\n"
-            f"`Duration:` {time_formatter(elapsed_time)}"
+            f"{progress_str}\n"
+            f"`{humanbytes(current)} of {humanbytes(total)}"
+            f" @ {humanbytes(speed)}`\n"
+            f"`ETA` -> {time_formatter(eta)}\n"
+            f"`Duration` -> {time_formatter(elapsed_time)}"
         )
-        await gdrive.edit(f"`{prog_type}`\n\n" f"`Status`\n{tmp}")
+        try:
+            await event.edit(f"`{prog_type}`\n\n"
+                             f"`Status`\n{tmp}")
+        except MessageNotModifiedError:
+            pass
