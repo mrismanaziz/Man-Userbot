94bb6ed7b28cbbbcbbb15250ec4e4b8cb97312eb
xz5pd <42339816+xz5pd@users.noreply.github.com>
2021-04-17 16:41:58 +0700

    Update termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -34,7 +34,7 @@ clear
 pkg update && pkg upgrade -y
 pkg install clang curl git libcrypt libffi libiconv libjpeg* libjpeg-turbo libwebp libxml2 libxslt make ndk-sysroot openssl postgresql python readline wget zlib -y
 
-git clone https://github.com/mrismanaziz/Man-Userbot.git
+git clone https://github.com/xz5pd/Man-Userbot.git
 cd Man-UserBot
 
 pip install --upgrade pip setuptools
