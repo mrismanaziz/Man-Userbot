7e78d4b5b9c9b083d0eda83be211600366b906a7
xz5pd <42339816+xz5pd@users.noreply.github.com>
2021-04-17 17:15:17 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -96,7 +96,7 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**✘ MAN-USERBOT ERROR REPORT ✘**\n\n"
+                    text = "** MAN-USERBOT ERROR REPORT **\n\n"
                     link = "[Group Support](https://t.me/GrupCinta)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
