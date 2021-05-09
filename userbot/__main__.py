5fce0315a4f3dbff14d84508f4d13b37dd3edc93
pockybear28 <83899890+pockybear28@users.noreply.github.com>
2021-05-10 06:39:34 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"Man-Userbot ⚙️ V{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
+    f"SULTAN-Userbot ⚙️ V{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
 
 
 if len(argv) not in (1, 3, 4):
