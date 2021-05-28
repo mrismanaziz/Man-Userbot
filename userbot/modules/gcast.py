567b306b3094229c27f5e67cf8a39d5b3fcf1935
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-05-28 10:00:54 +0700

    gcast: add gucast modules

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -1,5 +1,11 @@
-# frm Ultroid
-# port by Koala @manusiarakitann
+# Ultroid - UserBot
+# Copyright (C) 2020 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+#
+# Ported by Koala @manusiarakitann
 # @LordUserbot_Group
 # @sharinguserbot
 
@@ -30,11 +36,44 @@ async def gcast(event):
     )
 
 
+@register(outgoing=True, pattern=r"^\.gucast(?: |$)(.*)")
+async def gucast(event):
+    xx = event.pattern_match.group(1)
+    if not xx:
+        return await event.edit("`Berikan Sebuah Pesan`")
+    tt = event.text
+    msg = tt[7:]
+    kk = await event.edit("`Globally Broadcasting Msg...`")
+    er = 0
+    done = 0
+    async for x in bot.iter_dialogs():
+        if x.is_user and not x.entity.bot:
+            chat = x.id
+            try:
+                done += 1
+                await bot.send_message(chat, msg)
+            except BaseException:
+                er += 1
+    await kk.edit(
+        f"**Berhasil Mengirim Pesan Ke** {done} **chats**, Gagal Mengirim Pesan Ke** {er} **chats**"
+    )
+
+
 CMD_HELP.update(
     {
         "gcast": "**Plugin : **`gcast`\
         \n\n  •  **Syntax :** `.gcast` <text>`\
         \n  •  **Function : **Mengirim  Global Broadcast pesan ke Seluruh Grup yang kamu masuk.\
+    "
+    }
+)
+
+
+CMD_HELP.update(
+    {
+        "gucast": "**Plugin : **`gucast`\
+        \n\n  •  **Syntax :** `.gucast` <text>`\
+        \n  •  **Function : **Mengirim  Global Broadcast pesan ke Seluruh Private Massage / PC yang masuk.\
     "
     }
 )
