48aed1cd108826b65056d1a376d0bba133f7a261
mrismanaziz <mrismanaziz@gmail.com>
2021-06-03 17:09:17 +0000

    pylint: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -2,11 +2,11 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
-# 
+#
 # inline credit @keselekpermen69
 # Recode by @mrismanaziz
 # t.me/SharingUserbot
-# 
+#
 """ Userbot initialization. """
 
 import os
