a7be4fa1442a58ab9cbb827fb77809a7b4cad741
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-06-13 15:40:19 +0700

    fakeaction: add auto delete message

--- userbot/modules/fakeaction.py
+++ userbot/modules/fakeaction.py
@@ -29,6 +29,8 @@ async def _(event):
             except BaseException:
                 return await event.edit("`Masukan jumlah detik yang benar`")
     await event.edit(f"**Memulai Pengetikan Palsu Selama** `{t}` **detik.**")
+    await asyncio.sleep(3)
+    await event.delete()
     async with event.client.action(event.chat_id, "typing"):
         await asyncio.sleep(t)
 
@@ -47,6 +49,8 @@ async def _(event):
             except BaseException:
                 return await event.edit("`Masukan jumlah detik yang benar`")
     await event.edit(f"**Memulai merekam audio palsu Selama** `{t}` **detik.**")
+    await asyncio.sleep(3)
+    await event.delete()
     async with event.client.action(event.chat_id, "record-audio"):
         await asyncio.sleep(t)
 
@@ -65,6 +69,8 @@ async def _(event):
             except BaseException:
                 return await event.edit("`Masukan jumlah detik yang benar`")
     await event.edit(f"**Memulai merekam video palsu Selama** `{t}` **detik.**")
+    await asyncio.sleep(3)
+    await event.delete()
     async with event.client.action(event.chat_id, "record-video"):
         await asyncio.sleep(t)
 
@@ -83,6 +89,8 @@ async def _(event):
             except BaseException:
                 return await event.edit("`Masukan jumlah detik yang benar`")
     await event.edit(f"**Memulai Bermain Game Palsu Selama** `{t}` **detik.**")
+    await asyncio.sleep(3)
+    await event.delete()
     async with event.client.action(event.chat_id, "game"):
         await asyncio.sleep(t)
 
