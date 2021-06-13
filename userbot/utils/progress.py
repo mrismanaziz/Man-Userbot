987a0206331d1e6a9cfc7cd37bd6b6088b6f6419
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-06-13 14:36:27 +0700

    progress: add file name and status

--- userbot/utils/progress.py
+++ userbot/utils/progress.py
@@ -25,8 +25,7 @@ from .exceptions import CancelProcess
 
 
 async def progress(
-    current, total, event, start, prog_type,
-    file_name=None, is_cancelled=False
+    current, total, gdrive, start, prog_type, file_name=None, is_cancelled=False
 ):
     now = time.time()
     diff = now - start
@@ -38,28 +37,38 @@ async def progress(
         speed = current / diff
         elapsed_time = round(diff)
         eta = round((total - current) / speed)
-        if 'upload' in prog_type.lower():
-            status = 'Uploading'
-        elif 'download' in prog_type.lower():
-            status = 'Downloading'
+        if "upload" in prog_type.lower():
+            status = "Uploading"
+        elif "download" in prog_type.lower():
+            status = "Downloading"
         else:
-            status = 'Unknown'
-        progress_str = "`{0}` | [{1}{2}] `{3}%`".format(
+            status = "Unknown"
+        progress_str = "`{0}` | `[{1}{2}] {3}%`".format(
             status,
-            ''.join(["●" for i in range(
-                    math.floor(percentage / 10))]),
-            ''.join(["○" for i in range(
-                    10 - math.floor(percentage / 10))]),
-            round(percentage, 2))
+            "".join(["●" for i in range(math.floor(percentage / 10))]),
+            "".join(["○" for i in range(10 - math.floor(percentage / 10))]),
+            round(percentage, 2),
+        )
         tmp = (
             f"{progress_str}\n"
             f"`{humanbytes(current)} of {humanbytes(total)}"
             f" @ {humanbytes(speed)}`\n"
-            f"`ETA` -> {time_formatter(eta)}\n"
-            f"`Duration` -> {time_formatter(elapsed_time)}"
+            f"**ETA :**` {time_formatter(eta)}`\n"
+            f"**Duration :** `{time_formatter(elapsed_time)}`"
         )
         try:
-            await event.edit(f"`{prog_type}`\n\n"
-                             f"`Status`\n{tmp}")
+            if file_name:
+                await gdrive.edit(
+                    f"**{prog_type}**\n\n"
+                    f"**Nama File : **`{file_name}`**\nStatus**\n{tmp}"
+                )
+            else:
+                await gdrive.edit(f"**{prog_type}**\n\n" f"**Status**\n{tmp}")
         except MessageNotModifiedError:
             pass
+
+
+class CancelProcess(Exception):
+    """
+    Cancel Process
+    """
