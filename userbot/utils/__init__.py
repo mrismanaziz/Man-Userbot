d240d2c4cb1b4941b360c4d3d17878398658b030
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-06-13 14:38:55 +0700

    utils: progress add CancelProcess

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -16,5 +16,5 @@
 from .format import parse_pre
 from .chrome import chrome, options
 from .google_images_download import googleimagesdownload
-from .progress import progress
+from .progress import progress, CancelProcess
 from .tools import humanbytes, time_formatter, human_to_bytes, md5, take_screen_shot, runcmd, post_to_telegraph, run_cmd
