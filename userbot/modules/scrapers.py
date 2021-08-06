b8f25c40b3422f56d6d8ba4f0eacfae12133ab95
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-08-06 15:49:54 +0700

    scrapers: move paste modules and more

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -37,7 +37,7 @@ from googletrans import LANGUAGES, Translator
 from gtts import gTTS
 from gtts.lang import tts_langs
 from humanize import naturalsize
-from requests import exceptions, get, post
+from requests import get
 from search_engine_parser import YahooSearch as GoogleSearch
 from telethon.tl.types import DocumentAttributeAudio, MessageMediaPhoto
 from wikipedia import summary
@@ -102,10 +102,6 @@ async def ocr_space_file(
             data=payload,
         )
     return r.json()
-
-
-DOGBIN_URL = "https://del.dog/"
-NEKOBIN_URL = "https://nekobin.com/"
 
 
 @register(outgoing=True, pattern=r"^\.crblang (.*)")
@@ -635,7 +631,7 @@ def deEmojify(inputString):
     return get_emoji_regexp().sub("", inputString)
 
 
-@register(outgoing=True, pattern="^.rbg(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.rbg(?: |$)(.*)")
 async def kbg(remob):
     """For .rbg command, Remove Image Background."""
     if REM_BG_API_KEY is None:
@@ -724,7 +720,7 @@ async def ReTrieveURL(input_url):
     )
 
 
-@register(pattern=r".ocr (.*)", outgoing=True)
+@register(pattern=r"^\.ocr (.*)", outgoing=True)
 async def ocr(event):
     if not OCR_SPACE_API_KEY:
         return await event.edit(
@@ -749,7 +745,7 @@ async def ocr(event):
     os.remove(downloaded_file_name)
 
 
-@register(pattern=r"^.decode$", outgoing=True)
+@register(pattern=r"^\.decode$", outgoing=True)
 async def parseqr(qr_e):
     """For .decode command, get QR Code/BarCode content from the replied photo."""
     downloaded_file_name = await qr_e.client.download_media(
@@ -784,7 +780,7 @@ async def parseqr(qr_e):
     await qr_e.edit(qr_contents)
 
 
-@register(pattern=r".barcode(?: |$)([\s\S]*)", outgoing=True)
+@register(pattern=r"^\.barcode(?: |$)([\s\S]*)", outgoing=True)
 async def bq(event):
     """For .barcode command, genrate a barcode containing the given content."""
     await event.edit("`Processing..`")
@@ -819,7 +815,7 @@ async def bq(event):
     await event.delete()
 
 
-@register(pattern=r".makeqr(?: |$)([\s\S]*)", outgoing=True)
+@register(pattern=r"^\.makeqr(?: |$)([\s\S]*)", outgoing=True)
 async def make_qr(makeqr):
     """For .makeqr command, make a QR Code containing the given content."""
     input_str = makeqr.pattern_match.group(1)
@@ -857,7 +853,7 @@ async def make_qr(makeqr):
     await makeqr.delete()
 
 
-@register(outgoing=True, pattern=r"^.direct(?: |$)([\s\S]*)")
+@register(outgoing=True, pattern=r"^\.direct(?: |$)([\s\S]*)")
 async def direct_link_generator(request):
     """direct links generator"""
     await request.edit("`Processing...`")
@@ -1158,154 +1154,6 @@ def useragent():
     return user_agent.text
 
 
-@register(outgoing=True, pattern=r"^\.paste(?: |$)([\s\S]*)")
-async def paste(pstl):
-    dogbin_final_url = ""
-    match = pstl.pattern_match.group(1).strip()
-    reply_id = pstl.reply_to_msg_id
-
-    if not match and not reply_id:
-        return await pstl.edit("`Elon Musk said I cannot paste void.`")
-
-    if match:
-        message = match
-    elif reply_id:
-        message = await pstl.get_reply_message()
-        if message.media:
-            downloaded_file_name = await pstl.client.download_media(
-                message,
-                TEMP_DOWNLOAD_DIRECTORY,
-            )
-            m_list = None
-            with open(downloaded_file_name, "rb") as fd:
-                m_list = fd.readlines()
-            message = "".join(m.decode("UTF-8") for m in m_list)
-            os.remove(downloaded_file_name)
-        else:
-            message = message.message
-
-    # Dogbin
-    await pstl.edit("`Pasting text . . .`")
-    resp = post(DOGBIN_URL + "documents", data=message.encode("utf-8"))
-
-    if resp.status_code == 200:
-        response = resp.json()
-        key = response["key"]
-        dogbin_final_url = DOGBIN_URL + key
-
-        if response["isUrl"]:
-            reply_text = (
-                "`Pasted successfully!`\n\n"
-                f"[Shortened URL]({dogbin_final_url})\n\n"
-                "`Original(non-shortened) URLs`\n"
-                f"[Dogbin URL]({DOGBIN_URL}v/{key})\n"
-                f"[View RAW]({DOGBIN_URL}raw/{key})"
-            )
-        else:
-            reply_text = (
-                "`Pasted successfully!`\n\n"
-                f"[Dogbin URL]({dogbin_final_url})\n"
-                f"[View RAW]({DOGBIN_URL}raw/{key})"
-            )
-    else:
-        reply_text = "`Failed to reach Dogbin`"
-
-    await pstl.edit(reply_text)
-
-
-@register(outgoing=True, pattern=r"^\.getpaste(?: |$)(.*)")
-async def get_dogbin_content(dog_url):
-    textx = await dog_url.get_reply_message()
-    message = dog_url.pattern_match.group(1)
-    await dog_url.edit("`Getting dogbin content...`")
-
-    if textx:
-        message = str(textx.message)
-
-    format_normal = f"{DOGBIN_URL}"
-    format_view = f"{DOGBIN_URL}v/"
-
-    if message.startswith(format_view):
-        message = message[len(format_view) :]
-    elif message.startswith(format_normal):
-        message = message[len(format_normal) :]
-    elif message.startswith("del.dog/"):
-        message = message[len("del.dog/") :]
-    else:
-        return await dog_url.edit("`Is that even a dogbin url?`")
-
-    resp = get(f"{DOGBIN_URL}raw/{message}")
-
-    try:
-        resp.raise_for_status()
-    except exceptions.HTTPError as HTTPErr:
-        await dog_url.edit(
-            "Request returned an unsuccessful status code.\n\n" + str(HTTPErr)
-        )
-        return
-    except exceptions.Timeout as TimeoutErr:
-        await dog_url.edit("Request timed out." + str(TimeoutErr))
-        return
-    except exceptions.TooManyRedirects as RedirectsErr:
-        await dog_url.edit(
-            "Request exceeded the configured number of maximum redirections."
-            + str(RedirectsErr)
-        )
-        return
-
-    reply_text = (
-        "`Fetched dogbin URL content successfully!`" "\n\n`Content:` " + resp.text
-    )
-
-    await dog_url.edit(reply_text)
-
-
-@register(outgoing=True, pattern=r"^\.neko(?: |$)([\s\S]*)")
-async def neko(nekobin):
-    """For .paste command, pastes the text directly to dogbin."""
-    nekobin_final_url = ""
-    match = nekobin.pattern_match.group(1).strip()
-    reply_id = nekobin.reply_to_msg_id
-
-    if not match and not reply_id:
-        return await pstl.edit("`Cannot paste text.`")
-
-    if match:
-        message = match
-    elif reply_id:
-        message = await nekobin.get_reply_message()
-        if message.media:
-            downloaded_file_name = await nekobin.client.download_media(
-                message,
-                TEMP_DOWNLOAD_DIRECTORY,
-            )
-            m_list = None
-            with open(downloaded_file_name, "rb") as fd:
-                m_list = fd.readlines()
-            message = "".join(m.decode("UTF-8") for m in m_list)
-            os.remove(downloaded_file_name)
-        else:
-            message = message.text
-
-    # Nekobin
-    await nekobin.edit("`Pasting text . . .`")
-    resp = post(NEKOBIN_URL + "api/documents", json={"content": message})
-
-    if resp.status_code == 201:
-        response = resp.json()
-        key = response["result"]["key"]
-        nekobin_final_url = NEKOBIN_URL + key
-        reply_text = (
-            "`Pasted successfully!`\n\n"
-            f"[Nekobin URL]({nekobin_final_url})\n"
-            f"[View RAW]({NEKOBIN_URL}raw/{key})"
-        )
-    else:
-        reply_text = "`Gagal menjangkau Nekobin`"
-
-    await nekobin.edit(reply_text)
-
-
 @register(pattern=r"^\.ss (.*)", outgoing=True)
 async def capture(url):
     """For .ss command, capture a website's screenshot and send the photo."""
@@ -1377,20 +1225,6 @@ CMD_HELP.update(
         \n\n  •  **Syntax :** `.tr` <text/reply>\
         \n  •  **Function : **Menerjemahkan teks ke bahasa yang disetel.\
         \n\n  •  **NOTE :** Gunakan .lang tr <kode bahasa> untuk menyetel bahasa untuk tr **(Bahasa Default adalah bahasa Indonesia)**\
-    "
-    }
-)
-
-
-CMD_HELP.update(
-    {
-        "paste": "**Plugin : **`paste`\
-        \n\n  •  **Syntax :** `.paste` <text/reply>\
-        \n  •  **Function : **Buat paste atau url yang dipersingkat menggunakan dog in.\
-        \n\n  •  **Syntax :** `.getpaste` <text/reply>\
-        \n  •  **Function : **Buat paste atau url yang dipersingkat menggunakan dog in.\
-        \n\n  •  **Syntax :** `.neko` <text/reply>\
-        \n  •  **Function : **Buat paste atau url yang dipersingkat menggunakan dog in.\
     "
     }
 )
