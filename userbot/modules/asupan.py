f995fee80df9b752f862bdb548bb794572ed2e1b
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-08-16 18:35:39 +0700

    Man-Userbot: add asupan modules
    Co-authored-by: Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
    Co-authored-by: mrismanaziz <mrismanaziz@gmail.com>

---
+++ userbot/modules/asupan.py
@@ -0,0 +1,51 @@
+# 🍀 © @tofik_dn
+# ⚠️ Do not remove credits
+
+import requests
+
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern=r"^\.asupan$")
+async def _(event):
+    try:
+        response = requests.get("https://tede-api.herokuapp.com/api/asupan/ptl").json()
+        await event.client.send_file(event.chat_id, response["url"])
+        await event.delete()
+    except Exception:
+        await event.edit("**Tidak bisa menemukan video asupan.**")
+
+
+@register(outgoing=True, pattern=r"^\.wibu$")
+async def _(event):
+    try:
+        response = requests.get("https://tede-api.herokuapp.com/api/asupan/wibu").json()
+        await event.client.send_file(event.chat_id, response["url"])
+        await event.delete()
+    except Exception:
+        await event.edit("**Tidak bisa menemukan video wibu.**")
+
+
+@register(outgoing=True, pattern=r"^\.chika$")
+async def _(event):
+    try:
+        response = requests.get("https://tede-api.herokuapp.com/api/chika").json()
+        await event.client.send_file(event.chat_id, response["url"])
+        await event.delete()
+    except Exception:
+        await event.edit("**Tidak bisa menemukan video chikakiku.**")
+
+
+CMD_HELP.update(
+    {
+        "asupan": "**Plugin : **`asupan`\
+        \n\n  •  **Syntax :** `.asupan`\
+        \n  •  **Function : **Untuk Mengirim video asupan secara random.\
+        \n\n  •  **Syntax :** `.wibu`\
+        \n  •  **Function : **Untuk Mengirim video wibu secara random.\
+        \n\n  •  **Syntax :** `.chika`\
+        \n  •  **Function : **Untuk Mengirim video chikakiku secara random.\
+    "
+    }
+)
