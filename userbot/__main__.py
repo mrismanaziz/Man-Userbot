11fd0855de182d3b9a0aa5a09e709524a4fcfebf
ABKeceX <89113382+ABKeceX@users.noreply.github.com>
2021-09-14 02:30:36 +0800

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"COKK-UserBot ⚙️ V{BOT_VER} [🔥 BERHASIL DIAKTIFKAN COK! 🔥]")
+    f"COKK-UserBot ⚙️ V{BOT_VER} [乂 COKK-USERBOT SUDAH AKTIF COK! 乂]")
 
 
 if len(argv) not in (1, 3, 4):
