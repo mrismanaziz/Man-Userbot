14cbf3e733d6b949bc89747d2ccb380caa34dd2e
mrismanaziz <mrismanaziz@gmail.com>
2021-10-13 14:09:44 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/oi.py
+++ userbot/modules/oi.py
@@ -1,7 +1,8 @@
 from time import sleep
 
+from userbot import BLACKLIST_CHAT
 from userbot import CMD_HANDLER as cmd
-from userbot import BLACKLIST_CHAT, CMD_HELP, bot
+from userbot import CMD_HELP, bot
 from userbot.events import man_cmd
 
 
