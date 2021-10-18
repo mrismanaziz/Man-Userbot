5cd53cb31adb9561fd3fe16f7cc87910b39e4f48
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-10-19 03:58:48 +0700

    markdown: remove mention because already exists
    already here: https://github.com/mrismanaziz/Man-Userbot/blob/f8971ebea68bc171ef96de6aa2262a24dfcc685e/userbot/modules/mentions.py#L21

--- userbot/modules/markdown.py
+++ userbot/modules/markdown.py
@@ -7,7 +7,6 @@ from random import choice
 
 from telethon import events
 from telethon.extensions.markdown import DEFAULT_URL_RE
-from telethon.tl import types
 from telethon.tl.functions.messages import EditMessageRequest
 from telethon.tl.types import (
     MessageEntityBold,
@@ -177,35 +176,3 @@ async def reparse(event):
             )
         )
         raise events.StopPropagation
-
-
-@register(outgoing=True)
-async def mention(event):
-    newstr = event.text
-    if event.entities:
-        newstr = nameexp.sub(r'<a href="tg://user?id=\2">\3</a>', newstr, 0)
-        for match in usernexp.finditer(newstr):
-            user = match.group(1)
-            text = match.group(2)
-            name, entities = await event.client._parse_message_text(text, "md")
-            rep = f'<a href="tg://resolve?domain={user}">{name}</a>'
-            if entities:
-                for e in entities:
-                    tag = None
-                    if isinstance(e, types.MessageEntityBold):
-                        tag = "<b>{}</b>"
-                    elif isinstance(e, types.MessageEntityItalic):
-                        tag = "<i>{}</i>"
-                    elif isinstance(e, types.MessageEntityCode):
-                        tag = "<code>{}</code>"
-                    elif isinstance(e, types.MessageEntityStrike):
-                        tag = "<s>{}</s>"
-                    elif isinstance(e, types.MessageEntityPre):
-                        tag = "<pre>{}</pre>"
-                    elif isinstance(e, types.MessageEntityUnderline):
-                        tag = "<u>{}</u>"
-                    if tag:
-                        rep = tag.format(rep)
-            newstr = re.sub(re.escape(match.group(0)), rep, newstr)
-    if newstr != event.text:
-        await event.edit(newstr, parse_mode="html")
