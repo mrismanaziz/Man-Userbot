d4763b5ee629d87d577a9fa856ff2c808d6fd018
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-11-05 01:08:30 +0700

    lyrics: rewrite

--- userbot/modules/lyrics.py
+++ userbot/modules/lyrics.py
@@ -1,68 +1,37 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
+# Credits: @mrismanaziz
+# API by @tofik_dn || https://github.com/tofikdn
+# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
+# t.me/SharingUserbot & t.me/Lunatic0de
 
-import os
-
-import lyricsgenius
-from pylast import User
+import requests
 
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, GENIUS, LASTFM_USERNAME, bot, lastfm
+from userbot import CMD_HELP, bot
 from userbot.events import man_cmd
-
-if GENIUS is not None:
-    genius = lyricsgenius.Genius(GENIUS)
+from userbot.utils import edit_or_reply
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"lyrics (?:(now)|(.*) - (.*))"))
-async def lyrics(lyric):
-    await lyric.edit("`Getting information...`")
-    if GENIUS is None:
-        await lyric.edit("`Provide genius access token to Heroku ConfigVars...`")
-        return False
-    if lyric.pattern_match.group(1) == "now":
-        playing = User(LASTFM_USERNAME, lastfm).get_now_playing()
-        if playing is None:
-            await lyric.edit("`No information current lastfm scrobbling...`")
-            return False
-        artist = playing.get_artist()
-        song = playing.get_title()
-    else:
-        artist = lyric.pattern_match.group(2)
-        song = lyric.pattern_match.group(3)
-    await lyric.edit(f"`Searching lyrics for {artist} - {song}...`")
-    songs = genius.search_song(song, artist)
-    if songs is None:
-        await lyric.edit(f"`Song`  **{artist} - {song}**  `not found...`")
-        return False
-    if len(songs.lyrics) > 4096:
-        await lyric.edit("`Lyrics is too big, view the file to see it.`")
-        with open("lyrics.txt", "w+") as f:
-            f.write(f"Search query: \n{artist} - {song}\n\n{songs.lyrics}")
-        await lyric.client.send_file(
-            lyric.chat_id,
-            "lyrics.txt",
-            reply_to=lyric.id,
-        )
-        os.remove("lyrics.txt")
-    else:
-        await lyric.edit(
-            f"**Search query**:\n`{artist}` - `{song}`" f"\n\n```{songs.lyrics}```"
-        )
-
-    return True
+@bot.on(man_cmd(outgoing=True, pattern=r"lyrics(?:\s|$)([\s\S]*)"))
+async def _(event):
+    query = event.pattern_match.group(1)
+    if not query:
+        return await event.edit("**Silahkan Masukan Judul Lagu**")
+    try:
+        xxnx = await edit_or_reply(event, "`Searching Lyrics...`")
+        respond = requests.get(
+            f"https://api-tede.herokuapp.com/api/lirik?l={query}"
+        ).json()
+        result = f"{respond['data']}"
+        await xxnx.edit(result)
+    except Exception:
+        await xxnx.edit(f"**Lirik lagu tidak ditemukan.**")
 
 
 CMD_HELP.update(
     {
         "lyrics": f"**Plugin : **`lyrics`\
-        \n\n  •  **Syntax :** `{cmd}lyrics` **<nama artist> - <nama lagu>**\
-        \n  •  **Function : **Dapatkan lirik yang cocok dengan artis dan lagu.\
-        \n\n  •  **Syntax :** `{cmd}lyrics now`\
-        \n  •  **Function : **Dapatkan lirik artis dan lagu dari scrobbling lastfm saat ini.\
+        \n\n  •  **Syntax :** `{cmd}lyrics` <judul lagu>\
+        \n  •  **Function : **Dapatkan lirik lagu yang cocok dengan judul lagu.\
     "
     }
 )
