b0495a8d8df3cbdcc5c16452ffd4d85075a6663c
mrismanaziz <mrismanaziz@gmail.com>
2021-11-08 17:33:48 +0700

    Man-Userbot: core: update
    *workflows: add pylint to core file
    * add core for pmbot
    
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

---
+++ userbot/core/decorators.py
@@ -0,0 +1,33 @@
+import asyncio
+
+from telethon.errors import FloodWaitError, MessageNotModifiedError
+from telethon.events import CallbackQuery
+
+from userbot import ALIVE_NAME, SUDO_USERS, bot
+from userbot.modules.sql_helper.globals import gvarstatus
+
+
+def check_owner(func):
+    async def wrapper(c_q: CallbackQuery):
+        user = await bot.get_me()
+        uid = user.id
+        if c_q.query.user_id and (
+            c_q.query.user_id == uid or c_q.query.user_id in SUDO_USERS
+        ):
+            try:
+                await func(c_q)
+            except FloodWaitError as e:
+                await asyncio.sleep(e.seconds + 5)
+            except MessageNotModifiedError:
+                pass
+        else:
+            HELP_TEXT = (
+                gvarstatus("HELP_TEXT")
+                or f"Hanya {ALIVE_NAME} yang dapat Mengakses Ini!!\nSilahkan Buat UserBot Anda sendiri."
+            )
+            await c_q.answer(
+                HELP_TEXT,
+                alert=True,
+            )
+
+    return wrapper
