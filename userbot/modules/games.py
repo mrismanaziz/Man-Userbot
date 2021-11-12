a02ae5f4adac41046f9db9269858e7add3b3105e
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-11-12 19:09:51 +0700

    games: truth_or_dare: switch to psycatgames API

---
+++ userbot/modules/games.py
@@ -0,0 +1,61 @@
+import json
+import random
+
+import requests
+
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP, bot
+from userbot.events import man_cmd
+from userbot.utils import edit_delete, edit_or_reply
+
+category = ["classic", "kids", "party", "hot", "mixed"]
+
+
+async def get_task(mode, choice):
+    url = "https://psycatgames.com/api/tod-v2/"
+    data = {
+        "id": "truth-or-dare",
+        "language": "id",
+        "category": category[choice],
+        "type": mode,
+    }
+    headers = {
+        "referer": "https://psycatgames.com/app/truth-or-dare/?utm_campaign=tod_website&utm_source=tod_id&utm_medium=website"
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    result = response.json()["results"]
+    return random.choice(result)
+
+
+@bot.on(man_cmd(outgoing=True, pattern=r"(task|truth|dare)(?: |$)([1-5]+)?$"))
+async def tod(event):
+    tod = event.pattern_match.group(1)
+    if tod == "task":
+        xxnx = await edit_or_reply(event, "`Processing...`")
+        tod = random.choice(["truth", "dare"])
+    else:
+        xxnx = await edit_or_reply(event, f"`Tugas {tod} acak untuk Anda...`")
+    category = event.pattern_match.group(2)
+    category = int(random.choice(category)) if category else random.choice([1, 2])
+    try:
+        task = await get_task(tod, category)
+        if tod == "truth":
+            await xxnx.edit(f"**Tugas Truth untuk Anda adalah**\n`{task}`")
+        else:
+            await xxnx.edit(f"**Tugas Dare untuk Anda adalah**\n`{task}`")
+    except Exception as e:
+        await edit_delete(xxnx, f"**ERROR: `{e}`")
+
+
+CMD_HELP.update(
+    {
+        "games": f"**Plugin : **`games`\
+        \n\n  •  **Syntax :** `{cmd}truth`\
+        \n  •  **Function : **Memberikan anda tantangan kejujuran secara random.\
+        \n\n  •  **Syntax :** `{cmd}dare`\
+        \n  •  **Function : **Memberikan anda tantangan Keberanian secara random.\
+        \n\n  •  **Syntax :** `{cmd}task`\
+        \n  •  **Function : **Untuk Memberikan anda tantangan secara random.\
+    "
+    }
+)
