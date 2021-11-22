d4f3fcaddc64e2db1e1f7f6b1c52170e93ef9df1
canskuy <71593098+cannskuy@users.noreply.github.com>
2021-11-23 03:19:49 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -153,7 +153,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL", "https://github.com/SonicXsap/Man-Userbot.git"
 )
-UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "Man-Userbot")
+UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "Sonic-Userbot")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
