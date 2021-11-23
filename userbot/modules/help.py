73b90cbc2ae0df1f7f1870080b9ec8c8697aa599
canskuy <71593098+cannskuy@users.noreply.github.com>
2021-11-24 00:57:54 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -30,9 +30,9 @@ async def help(event):
             string += f"`\t\t\t{ICON_HELP}\t\t\t"
         await edit_or_reply(
             event,
-            f"**✦ Daftar Perintah Untuk [Sonic-Userbot](https://github.com/SonicXsap/Man-Userbot):**\n"
-            f"**✦ Jumlah​** `{len(modules​)}` **Modules​**\n"
-            f"**✦ Owner​:** [{user.first_name}](tg://user?id={user.id})\n\n"
+            f"**✦ Daftar Perintah Untuk [Sonic-Userbot](https://github.com/SonicXsap/Sonic-Userbot):**\n"
+            f"**✦ Jumlah** `{len(modules)}` **Modules**\n"
+            f"**✦ Owner:** [{user.first_name}](tg://user?id={user.id})\n\n"
             f"{ICON_HELP}   {string}"
             f"\n\nSupport @{CHANNEL}",
         )
