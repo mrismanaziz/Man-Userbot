8e48e3db4674f22fe75f7973eda3d34b4028a14e
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-11-30 03:13:34 +0700

    stickers: add a command for kanging a whole pack

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -18,7 +18,9 @@ import requests
 from bs4 import BeautifulSoup as bs
 from PIL import Image
 from telethon import events
+from telethon.errors import PackShortNameOccupiedError
 from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl import functions, types
 from telethon.tl.functions.messages import GetStickerSetRequest
 from telethon.tl.types import (
     DocumentAttributeFilename,
@@ -26,13 +28,16 @@ from telethon.tl.types import (
     InputStickerSetID,
     MessageMediaPhoto,
 )
+from telethon.utils import get_input_document
 
+from userbot import BOT_USERNAME
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
 from userbot import S_PACK_NAME as custompack
-from userbot import bot
+from userbot import bot, tgbot, user
 from userbot.events import man_cmd
-from userbot.utils import edit_or_reply
+from userbot.modules.sql_helper.globals import addgvar, gvarstatus
+from userbot.utils import edit_delete, edit_or_reply
 
 KANGING_STR = [
     "Colong Sticker dulu yee kan",
@@ -41,6 +46,9 @@ KANGING_STR = [
     "ehh, keren nih... gua colong ya stickernya...",
     "Boleh juga ni Sticker Colong ahh~",
 ]
+
+OWNER = user.first_name
+OWNER_ID = user.id
 
 
 @bot.on(man_cmd(outgoing=True, pattern=r"(?:tikel|kang)\s?(.)?"))
@@ -285,6 +293,81 @@ async def resize_photo(photo):
     return image
 
 
+@bot.on(man_cmd(outgoing=True, pattern="pkang(?:\\s|$)([\\s\\S]*)"))
+async def _(event):
+    xnxx = await edit_or_reply(event, f"`{random.choice(KANGING_STR)}`")
+    reply = await event.get_reply_message()
+    query = event.text[7:]
+    bot_ = BOT_USERNAME
+    bot_un = bot_.replace("@", "")
+    user = await event.client.get_me()
+    un = f"@{user.username}" if user.username else user.first_name
+    un_ = user.username if user.username else OWNER_ID
+    if not reply:
+        return await edit_delete(
+            xnxx, "**Mohon Balas sticker untuk mencuri semua Sticker Pack itu.**"
+        )
+    if query == "":
+        pname = f"{un} Sticker Pack"
+    else:
+        pname = query
+    if reply and reply.media and reply.media.document.mime_type == "image/webp":
+        tikel_id = reply.media.document.attributes[1].stickerset.id
+        tikel_hash = reply.media.document.attributes[1].stickerset.access_hash
+        got_stcr = await event.client(
+            functions.messages.GetStickerSetRequest(
+                stickerset=types.InputStickerSetID(id=tikel_id, access_hash=tikel_hash)
+            )
+        )
+        stcrs = []
+        for sti in got_stcr.documents:
+            inp = get_input_document(sti)
+            stcrs.append(
+                types.InputStickerSetItem(
+                    document=inp,
+                    emoji=(sti.attributes[1]).alt,
+                )
+            )
+        try:
+            gvarstatus("PKANG")
+        except BaseException:
+            addgvar("PKANG", "0")
+        x = gvarstatus("PKANG")
+        try:
+            pack = int(x) + 1
+        except BaseException:
+            pack = 1
+        await xnxx.edit(f"`{random.choice(KANGING_STR)}`")
+        try:
+            create_st = await tgbot(
+                functions.stickers.CreateStickerSetRequest(
+                    user_id=OWNER_ID,
+                    title=pname,
+                    short_name=f"man_{un_}_V{pack}_by_{bot_un}",
+                    stickers=stcrs,
+                )
+            )
+            addgvar("PKANG", str(pack))
+        except PackShortNameOccupiedError:
+            await asyncio.sleep(1)
+            await xnxx.edit("`Sedang membuat paket baru...`")
+            pack += 1
+            create_st = await tgbot(
+                functions.stickers.CreateStickerSetRequest(
+                    user_id=OWNER_ID,
+                    title=pname,
+                    short_name=f"man_{un_}_V{pack}_by_{bot_un}",
+                    stickers=stcrs,
+                )
+            )
+            addgvar("PKANG", str(pack))
+        await xnxx.edit(
+            f"**Berhasil Mencuri Sticker Pack,** [Klik Disini](t.me/addstickers/{create_st.set.short_name}) **Untuk Melihat Pack anda**"
+        )
+    else:
+        await xnxx.edit("**Berkas Tidak Didukung. Harap Balas ke stiker saja.**")
+
+
 @bot.on(man_cmd(outgoing=True, pattern=r"stickerinfo$"))
 async def get_pack_info(event):
     if not event.is_reply:
@@ -518,6 +601,8 @@ CMD_HELP.update(
         \n  •  **Function : **Balas .kang Ke Sticker Atau Gambar Untuk Menambahkan Ke Sticker Pack Mu\
         \n\n  •  **Syntax :** `{cmd}kang` [emoji] atau `{cmd}tikel` [emoji]\
         \n  •  **Function : **Balas {cmd}kang emoji Ke Sticker Atau Gambar Untuk Menambahkan dan costum emoji sticker Ke Pack Mu\
+        \n\n  •  **Syntax :** `{cmd}pkang` <nama sticker pack>\
+        \n  •  **Function : **Balas {cmd}pkang Ke Sticker Untuk Mencuri semua sticker pack tersebut\
         \n\n  •  **Syntax :** `{cmd}delsticker` <reply sticker>\
         \n  •  **Function : **Untuk Menghapus sticker dari Sticker Pack.\
         \n\n  •  **Syntax :** `{cmd}editsticker` <reply sticker> <emoji>\
