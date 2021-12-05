7ce1d265b71bec5198a3c4480f5132f5e82a190b
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-12-05 20:44:50 +0700

    gcast: change GCAST_BLACKLIST to list json

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -12,28 +12,16 @@
 
 import asyncio
 
+from requests import get
 from telethon.errors import FloodWaitError
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, DEVS
 from userbot.utils import edit_delete, edit_or_reply, man_cmd
 
-GCAST_BLACKLIST = [
-    -1001473548283,  # SharingUserbot
-    -1001433238829,  # TedeSupport
-    -1001476936696,  # AnosSupport
-    -1001327032795,  # UltroidSupport
-    -1001294181499,  # UserBotIndo
-    -1001419516987,  # VeezSupportGroup
-    -1001209432070,  # GeezSupportGroup
-    -1001296934585,  # X-PROJECT BOT
-    -1001481357570,  # UsergeOnTopic
-    -1001459701099,  # CatUserbotSupport
-    -1001109837870,  # TelegramBotIndonesia
-    -1001485393652,  # Programmers Hub
-    -1001354786862,  # DaisyXSupport
-    -1001109500936,  # Telethon Chat
-]
+GCAST_BLACKLIST = get(
+    "https://raw.githubusercontent.com/mrismanaziz/Reforestation/master/blacklistgcast.json"
+).json()
 
 
 @man_cmd(pattern="gcast(?: |$)(.*)")
