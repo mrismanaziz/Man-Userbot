29ce8a3c50a1299739dba89bfc4691afb8ce6213
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-12-11 22:48:13 +0700

    Update logo.py

--- userbot/modules/logo.py
+++ userbot/modules/logo.py
@@ -6,8 +6,7 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, owner
-from userbot.events import man_cmd
-from userbot.utils import edit_or_reply
+from userbot.utils import edit_or_reply, man_cmd
 
 
 @man_cmd(pattern=r"logo(?: |$)(.*)")
