73ad5440e03bb259116c14b3f814d640947de844
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-12-26 11:32:21 +0700

    Update hentai.py

--- userbot/modules/hentai.py
+++ userbot/modules/hentai.py
@@ -7,40 +7,45 @@
 
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.contacts import UnblockRequest
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, bot
-from userbot.events import man_cmd
+from userbot.utils import edit_or_reply, man_cmd
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"hentai(?: |$)(.*)"))
+@man_cmd(pattern="hentai(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
     link = event.pattern_match.group(1)
     chat = "@nHentaiBot"
-    await event.edit("```Processing```")
-    async with bot.conversation(chat) as conv:
+    xx = await edit_or_reply(event, "`Processing...`")
+    async with event.client.conversation(chat) as conv:
         try:
             response = conv.wait_event(
                 events.NewMessage(incoming=True, from_users=424466890)
             )
-            await bot.send_message(chat, link)
+            await event.client.send_message(chat, link)
             response = await response
         except YouBlockedUserError:
-            await event.reply("```Please unblock @nHentaiBot and try again```")
-            return
+            await event.client(UnblockRequest(chat))
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=424466890)
+            )
+            await event.client.send_message(chat, link)
+            response = await response
         if response.text.startswith("**Sorry I couldn't get manga from**"):
-            await event.edit("```I think this is not the right link```")
+            await xx.edit("**Saya pikir ini bukan link yang benar**")
         else:
             await event.delete()
-            await bot.send_message(event.chat_id, response.message)
+            await event.client.send_message(event.chat_id, response.message)
 
 
 CMD_HELP.update(
     {
         "hentai": f"**Plugin : **`hentai`\
-        \n\n  •  **Syntax :** `{cmd}hentai`\
+        \n\n  •  **Syntax :** `{cmd}hentai` <link nhentai>\
         \n  •  **Function : **Melihat nhentai di telegra.ph XD\
     "
     }
