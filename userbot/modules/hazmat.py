66393b6c7da55537d40033d257aa66041f2b04a1
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-12-26 11:52:08 +0700

    Update hazmat.py

--- userbot/modules/hazmat.py
+++ userbot/modules/hazmat.py
@@ -5,27 +5,28 @@
 import os
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.contacts import UnblockRequest
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
-from userbot.events import man_cmd
+from userbot.utils import edit_or_reply, man_cmd
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"hz(:? |$)(.*)?"))
+@man_cmd(pattern="hz(:? |$)(.*)?")
 async def _(hazmat):
-    await hazmat.edit("`Processing Hazmat...`")
+    xx = await edit_or_reply(hazmat, "`Processing Hazmat...`")
     level = hazmat.pattern_match.group(2)
     if hazmat.fwd_from:
         return
     if not hazmat.reply_to_msg_id:
-        await hazmat.edit("`Mohon Balas Ke Sticker/Gambar`")
+        await xx.edit("**Mohon Balas Ke Sticker/Gambar**")
         return
     reply_message = await hazmat.get_reply_message()
     if not reply_message.media:
-        await hazmat.edit("`Kata Bisa Menghancurkan Apapun`")
+        await xx.edit("`Kata Bisa Menghancurkan Apapun`")
         return
     chat = "@hazmat_suit_bot"
-    await hazmat.edit("```Perintah Hazmat Diaktifkan, Sedang Memproses.... ```")
+    await xx.edit("`Perintah Hazmat Diaktifkan, Sedang Memproses...`")
     message_id_to_reply = hazmat.message.reply_to_msg_id
     msg_reply = None
     async with hazmat.client.conversation(chat) as conv:
@@ -40,13 +41,22 @@ async def _(hazmat):
                 msg_reply = await conv.send_message(m, reply_to=msg.id)
                 r = await conv.get_response()
             response = await conv.get_response()
-            """ - don't spam notif - """
-            await bot.send_read_acknowledge(conv.chat_id)
+            await hazmat.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await hazmat.reply("`Buka Blokir` @hazmat_suit_bot `Lalu Coba Lagi`")
-            return
+            await event.client(UnblockRequest(chat))
+            msg = await conv.send_message(reply_message)
+            if level:
+                m = f"/hazmat {level}"
+                msg_reply = await conv.send_message(m, reply_to=msg.id)
+                r = await conv.get_response()
+            elif reply_message.gif:
+                m = "/hazmat"
+                msg_reply = await conv.send_message(m, reply_to=msg.id)
+                r = await conv.get_response()
+            response = await conv.get_response()
+            await hazmat.client.send_read_acknowledge(conv.chat_id)
         if response.text.startswith("I can't"):
-            await hazmat.edit("`Mohon Maaf, GIF Tidak Bisa...`")
+            await xx.edit("`Mohon Maaf, GIF Tidak Bisa...`")
             await hazmat.client.delete_messages(
                 conv.chat_id, [msg.id, response.id, r.id, msg_reply.id]
             )
@@ -60,7 +70,6 @@ async def _(hazmat):
             force_document=False,
             reply_to=message_id_to_reply,
         )
-        """ - cleanup chat after completed - """
         if msg_reply is not None:
             await hazmat.client.delete_messages(
                 conv.chat_id, [msg.id, msg_reply.id, r.id, response.id]
