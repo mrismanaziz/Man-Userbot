835f7c2bc90d6ccb3748a19fcb9e3ca98b34a33d
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-12-26 18:18:08 +0700

    Update quotly.py

--- userbot/modules/quotly.py
+++ userbot/modules/quotly.py
@@ -18,12 +18,11 @@ async def _(event):
     if event.fwd_from:
         return
     if not event.reply_to_msg_id:
-        await edit_delete(event, "**Mohon Balas ke Pesan**")
-        return
+        return await edit_delete(event, "**Mohon Balas ke Pesan**")
     reply_message = await event.get_reply_message()
     warna = event.pattern_match.group(1)
     chat = "@QuotLyBot"
-    await edit_or_reply(event, "`Processing...`")
+    xx = await edit_or_reply(event, "`Processing...`")
     async with event.client.conversation(chat) as conv:
         try:
             response = conv.wait_event(
@@ -36,17 +35,14 @@ async def _(event):
             response = await response
         except YouBlockedUserError:
             await event.client(UnblockRequest(chat))
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=1031952739)
-            )
             first = await conv.send_message(f"/qcolor {warna}")
             ok = await conv.get_response()
             await asyncio.sleep(2)
             second = await event.client.forward_messages(chat, reply_message)
             response = await response
         if response.text.startswith("Hi!"):
-            await edit_or_reply(
-                event, "**Mohon Menonaktifkan Pengaturan Privasi Forward Anda**"
+            await xx.edit(
+                "**Mohon Menonaktifkan Pengaturan Privasi Forward Anda**"
             )
         else:
             await event.delete()
