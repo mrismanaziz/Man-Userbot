999f90a0ac675be0b259743ecb2b6ab9dc42c649
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-12-26 18:05:45 +0700

    Update other.py

--- userbot/modules/other.py
+++ userbot/modules/other.py
@@ -51,20 +51,16 @@ async def _(event):
             response = conv.wait_event(
                 events.NewMessage(incoming=True, from_users=botid)
             )
-            msg = await bot.send_message(chat, link)
+            msg = await event.client.send_message(chat, link)
             response = await response
             await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
             await event.client(UnblockRequest(chat))
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=botid)
-            )
-            msg = await bot.send_message(chat, link)
+            msg = await event.client.send_message(chat, link)
             response = await response
             await event.client.send_read_acknowledge(conv.chat_id)
         except BaseException:
-            await edit_delete(event, "**Tidak dapat menemukan bot itu 🥺**", 10)
-
+            await edit_delete(event, "**Tidak dapat menemukan bot itu 🥺**")
         await edit_or_reply(event, f"**Pesan Terkirim:** `{link}`\n**Kepada: {chat}**")
         await event.client.send_message(event.chat_id, response.message)
         await event.client.send_read_acknowledge(event.chat_id)
@@ -167,9 +163,6 @@ async def _(event):
             await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
             await event.client(UnblockRequest("@SpamBot"))
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=178220800)
-            )
             await conv.send_message("/start")
             response = await response
             await event.client.send_read_acknowledge(conv.chat_id)
@@ -197,9 +190,6 @@ async def _(event):
             await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
             await event.client(UnblockRequest(chat))
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=272572121)
-            )
             await event.client.forward_messages(chat, reply_message)
             response = await response
             await event.client.send_read_acknowledge(conv.chat_id)
