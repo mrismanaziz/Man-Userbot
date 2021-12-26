63d8b406e5fa91a7ab560017e0d802f5f44fb1c3
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-12-26 11:07:29 +0700

    Update tiktok.py

--- userbot/modules/tiktok.py
+++ userbot/modules/tiktok.py
@@ -6,39 +6,42 @@
 # Lord Userbot
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.contacts import UnblockRequest
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, bot
-from userbot.events import man_cmd
+from userbot.utils import edit_delete, edit_or_reply, man_cmd
 
 # Alvin Gans
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"tiktok(?: |$)(.*)"))
+@man_cmd(pattern="tiktok(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
     d_link = event.pattern_match.group(1)
     if ".com" not in d_link:
-        await event.edit("**Berikan Link Video Tiktok Untuk Download Video Tiktok**")
+        await edit_delete(event, "**Berikan Link Video Tiktok Untuk Download Video Tiktok**")
     else:
-        await event.edit("```Video Sedang Diproses.....```")
+        await edit_or_reply(event, "`Video Sedang Diproses...`")
     chat = "@ttsavebot"
-    async with bot.conversation(chat) as conv:
+    async with event.client.conversation(chat) as conv:
         try:
             msg_start = await conv.send_message("/start")
             r = await conv.get_response()
             msg = await conv.send_message(d_link)
             details = await conv.get_response()
             video = await conv.get_response()
-            """ - don't spam notif - """
-            await bot.send_read_acknowledge(conv.chat_id)
+            await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await event.edit(
-                "**Error: Mohon Buka Blokir** @ttsavebot **Dan Coba Lagi!**"
-            )
-            return
-        await bot.send_file(event.chat_id, video)
+            await event.client(UnblockRequest(chat))
+            msg_start = await conv.send_message("/start")
+            r = await conv.get_response()
+            msg = await conv.send_message(d_link)
+            details = await conv.get_response()
+            video = await conv.get_response()
+            await event.client.send_read_acknowledge(conv.chat_id)
+        await event.client.send_file(event.chat_id, video)
         await event.client.delete_messages(
             conv.chat_id, [msg_start.id, r.id, msg.id, details.id, video.id]
         )
