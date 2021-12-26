78af7f8692064e50670e313aeb4c7a2dccc9b16f
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-12-26 11:29:03 +0700

    Update kamuii.py

--- userbot/modules/kamuii.py
+++ userbot/modules/kamuii.py
@@ -7,27 +7,28 @@
 import os
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.contacts import UnblockRequest
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
-from userbot.events import man_cmd
+from userbot.utils import edit_delete, edit_or_reply, man_cmd
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"kamuii(:? |$)([1-8])?"))
+@man_cmd(pattern=r"kamuii(:? |$)([1-8])?")
 async def _(fry):
-    await fry.edit("`Mengaktifkan Kekuatan Supersaya...`")
+    xx = await edit_or_reply(fry, "`Mengaktifkan Kekuatan Supersaya...`")
     level = fry.pattern_match.group(2)
     if fry.fwd_from:
         return
     if not fry.reply_to_msg_id:
-        await fry.edit("`Balas Di Sticker`")
+        await edit_delete(xx, "**Silahkan Balas Ke Media Foto**")
         return
     reply_message = await fry.get_reply_message()
     if not reply_message.media:
-        await fry.edit("`Gambar tidak di dukung`")
+        await edit_delete(xx, "**Gambar tidak di dukung**")
         return
     if reply_message.sender.bot:
-        await fry.edit("`Mohon Balas Di Sticker`")
+        await edit_delete(xx, "**Mohon Balas Ke Media**")
         return
     chat = "@image_deepfrybot"
     message_id_to_reply = fry.message.reply_to_msg_id
@@ -39,69 +40,18 @@ async def _(fry):
                 msg_level = await conv.send_message(m, reply_to=msg.id)
                 r = await conv.get_response()
             response = await conv.get_response()
-            """ - don't spam notif - """
-            await bot.send_read_acknowledge(conv.chat_id)
+            await fry.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await fry.reply("`Unblock Terlebih Dahulu BOT ` @image_deepfrybot`...`")
-            return
-        if response.text.startswith("Forward"):
-            await fry.edit("`Matikan Setelan Privasi Forward ...`")
-        else:
-            downloaded_file_name = await fry.client.download_media(
-                response.media, TEMP_DOWNLOAD_DIRECTORY
-            )
-            await fry.client.send_file(
-                fry.chat_id,
-                downloaded_file_name,
-                force_document=False,
-                reply_to=message_id_to_reply,
-            )
-            """ - cleanup chat after completed - """
-            try:
-                msg_level
-            except NameError:
-                await fry.client.delete_messages(conv.chat_id, [msg.id, response.id])
-            else:
-                await fry.client.delete_messages(
-                    conv.chat_id, [msg.id, response.id, r.id, msg_level.id]
-                )
-    await fry.delete()
-    return os.remove(downloaded_file_name)
-
-
-@bot.on(man_cmd(outgoing=True, pattern=r".df(:? |$)([1-8])?"))
-async def _(fry):
-    await fry.edit("`Sedang Dalam Proses......`")
-    level = fry.pattern_match.group(2)
-    if fry.fwd_from:
-        return
-    if not fry.reply_to_msg_id:
-        await fry.edit("`Mohon Balas Di Sticker`")
-        return
-    reply_message = await fry.get_reply_message()
-    if not reply_message.media:
-        await fry.edit("`Mohon Balas Di Sticker`")
-        return
-    if reply_message.sender.bot:
-        await fry.edit("`Mohon Balas Di Sticker`")
-        return
-    chat = "@image_deepfrybot"
-    message_id_to_reply = fry.message.reply_to_msg_id
-    async with fry.client.conversation(chat) as conv:
-        try:
+            await event.client(UnblockRequest(chat))
             msg = await conv.send_message(reply_message)
             if level:
                 m = f"/deepfry {level}"
                 msg_level = await conv.send_message(m, reply_to=msg.id)
                 r = await conv.get_response()
             response = await conv.get_response()
-            """ - don't spam notif - """
-            await bot.send_read_acknowledge(conv.chat_id)
-        except YouBlockedUserError:
-            await fry.reply("`Mohon Unblock` @image_deepfrybot`...`")
-            return
+            await fry.client.send_read_acknowledge(conv.chat_id)
         if response.text.startswith("Forward"):
-            await fry.edit("`Mohon Matikan Setelan Privasi Forward...`")
+            await xx.edit("**Silahkan Matikan Setelan Privasi Forward**")
         else:
             downloaded_file_name = await fry.client.download_media(
                 response.media, TEMP_DOWNLOAD_DIRECTORY
@@ -112,7 +62,6 @@ async def _(fry):
                 force_document=False,
                 reply_to=message_id_to_reply,
             )
-            """ - cleanup chat after completed - """
             try:
                 msg_level
             except NameError:
@@ -128,7 +77,7 @@ async def _(fry):
 CMD_HELP.update(
     {
         "kamuii": f"**Plugin : **`kamuii`\
-        \n\n  •  **Syntax :** `{cmd}kamuii` or `{cmd}kamuii` [level(1-8)]\
+        \n\n  •  **Syntax :** `{cmd}kamuii` atau `{cmd}kamuii` [level(1-8)]\
         \n  •  **Function : **Untuk mengubah foto/sticker menjadi penyok.\
     "
     }
