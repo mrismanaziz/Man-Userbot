8228426d7736804f36b0f77f9d709c7df403ea73
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-12-30 21:12:55 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -45,7 +45,7 @@ async def sleepybot(time):
         return
     counter = int(time.pattern_match.group(1))
     xx = await edit_or_reply(time, "`Saya mengantuk dan tertidur...`")
-    if BOTLOG:
+    if BOTLOG_CHATID:
         str_counter = time_formatter(counter)
         await time.client.send_message(
             BOTLOG_CHATID,
@@ -61,7 +61,7 @@ async def killdabot(event):
         return
     if event.sender_id in SUDO_USERS:
         return
-    if BOTLOG:
+    if BOTLOG_CHATID:
         await event.client.send_message(
             BOTLOG_CHATID,
             "**#SHUTDOWN** \n"
@@ -74,12 +74,12 @@ async def killdabot(event):
         sys.exit(0)
 
 
-@man_cmd(pattern=r"restart$")
+@man_cmd(pattern="restart$")
 async def killdabot(event):
     if event.sender_id in SUDO_USERS:
         return
     await edit_or_reply(event, "**Man-Userbot Berhasil di Restart**")
-    if BOTLOG:
+    if BOTLOG_CHATID:
         await event.client.send_message(
             BOTLOG_CHATID, "#RESTART \n" "**Man-Userbot Berhasil Di Restart**"
         )
@@ -166,7 +166,7 @@ async def okgoogle(img):
     message = await img.get_reply_message()
     if message and message.media:
         photo = io.BytesIO()
-        await bot.download_media(message, photo)
+        await img.client.download_media(message, photo)
     else:
         return await edit_or_reply(img, "**Harap Balas ke Gambar**")
     if photo:
@@ -221,12 +221,9 @@ async def okgoogle(img):
 
 async def ParseSauce(googleurl):
     """Parse/Scrape the HTML code for the info we want."""
-
     source = opener.open(googleurl).read()
     soup = BeautifulSoup(source, "html.parser")
-
     results = {"similar_images": "", "best_guess": ""}
-
     try:
         for similar_image in soup.findAll("input", {"class": "gLFyf"}):
             url = "https://www.google.com/search?tbm=isch&q=" + urllib.parse.quote_plus(
@@ -235,54 +232,42 @@ async def ParseSauce(googleurl):
             results["similar_images"] = url
     except BaseException:
         pass
-
     for best_guess in soup.findAll("div", attrs={"class": "r5a77d"}):
         results["best_guess"] = best_guess.get_text()
-
     return results
 
 
 async def scam(results, lim):
-
     single = opener.open(results["similar_images"]).read()
     decoded = single.decode("utf-8")
-
     imglinks = []
     counter = 0
-
     pattern = r"^,\[\"(.*[.png|.jpg|.jpeg])\",[0-9]+,[0-9]+\]$"
     oboi = re.findall(pattern, decoded, re.I | re.M)
-
     for imglink in oboi:
         counter += 1
         if counter < int(lim):
             imglinks.append(imglink)
         else:
             break
-
     return imglinks
 
 
 @man_cmd(pattern="send (.*)")
 async def send(event):
-    xx = await edit_or_reply(event, "**Berhasil Mengirim pesan ini**")
-
     if not event.is_reply:
-        return await xxnx = await edit_or_reply(event, "**Mohon Balas ke pesan yang ingin dikirim!**")
-
+        return await edit_or_reply(event, "**Mohon Balas ke pesan yang ingin dikirim!**")
     chat = event.pattern_match.group(1)
+    xx = await edit_or_reply(event, "**Berhasil Mengirim pesan ini**")
     try:
         chat = int(chat)
     except ValueError:
         pass
-
     try:
         chat = await event.client.get_entity(chat)
     except (TypeError, ValueError):
         return await xx.edit("**Link yang diberikan tidak valid!**")
-
     message = await event.get_reply_message()
-
     await event.client.send_message(entity=chat, message=message)
     await xx.edit(f"**Berhasil Mengirim pesan ini ke** `{chat.title}`")
 
