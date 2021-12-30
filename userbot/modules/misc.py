456afda73577a31f489101c677c36d821d420e0b
mrismanaziz <mrismanaziz@gmail.com>
2021-12-30 22:02:21 +0700

    misc: update and improvements

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -9,7 +9,6 @@
 # FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
 # t.me/SharingUserbot & t.me/Lunatic0de
 #
-""" Userbot module for other small commands. """
 
 import io
 import os
@@ -21,14 +20,14 @@ from time import sleep
 
 import requests
 from bs4 import BeautifulSoup
+from git import Repo
 from heroku3 import from_key
 from PIL import Image
 
-from userbot import BOT_VER, BOTLOG, BOTLOG_CHATID
+from userbot import BOT_VER, BOTLOG_CHATID
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, HEROKU_API_KEY, HEROKU_APP_NAME, UPSTREAM_REPO_BRANCH, bot
-from userbot.events import man_cmd
-from userbot.utils import time_formatter
+from userbot import CMD_HELP, HEROKU_API_KEY, HEROKU_APP_NAME, SUDO_USERS
+from userbot.utils import edit_or_reply, man_cmd, time_formatter
 
 # ================= CONSTANT =================
 HEROKU_APP = from_key(HEROKU_API_KEY).apps()[HEROKU_APP_NAME]
@@ -37,67 +36,74 @@ HEROKU_APP = from_key(HEROKU_API_KEY).apps()[HEROKU_APP_NAME]
 opener = urllib.request.build_opener()
 useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.70 Mobile Safari/537.36"
 opener.addheaders = [("User-agent", useragent)]
+repo = Repo()
+branch = repo.active_branch.name
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"sleep ([0-9]+)$"))
+@man_cmd(pattern="sleep ([0-9]+)$")
 async def sleepybot(time):
-    """For .sleep command, let the userbot snooze for a few second."""
+    if time.sender_id in SUDO_USERS:
+        return
     counter = int(time.pattern_match.group(1))
-    await time.edit("`Saya mengantuk dan tertidur...`")
-    if BOTLOG:
+    xx = await edit_or_reply(time, "`Saya mengantuk dan tertidur...`")
+    if BOTLOG_CHATID:
         str_counter = time_formatter(counter)
         await time.client.send_message(
             BOTLOG_CHATID,
-            f"Anda menyuruh bot untuk tidur {str_counter}.",
+            f"**Anda menyuruh bot untuk tidur selama** {str_counter}.",
         )
     sleep(counter)
-    await time.edit("**Oke, saya sudah bangun sekarang.**")
+    await xx.edit("**Oke, saya sudah bangun sekarang.**")
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"shutdown$"))
-async def killdabot(event):
+@man_cmd(pattern="shutdown$")
+async def shutdown_bot(event):
     if event.fwd_from:
         return
-    if BOTLOG:
+    if event.sender_id in SUDO_USERS:
+        return
+    if BOTLOG_CHATID:
         await event.client.send_message(
             BOTLOG_CHATID,
             "**#SHUTDOWN** \n"
             "**Man-Userbot** telah di matikan!\nJika ingin menghidupkan kembali silahkan buka heroku",
         )
-    await event.edit("`Man-Userbot Berhasil di matikan!`")
+    await edit_or_reply(event, "**Man-Userbot Berhasil di matikan!**")
     if HEROKU_APP is not None:
         HEROKU_APP.process_formation()["worker"].scale(0)
     else:
         sys.exit(0)
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"restart$"))
-async def killdabot(event):
-    await event.edit("**Man-Userbot Berhasil di Restart**")
-    if BOTLOG:
+@man_cmd(pattern="restart$")
+async def restart_bot(event):
+    if event.sender_id in SUDO_USERS:
+        return
+    await edit_or_reply(event, "**Man-Userbot Berhasil di Restart**")
+    if BOTLOG_CHATID:
         await event.client.send_message(
             BOTLOG_CHATID, "#RESTART \n" "**Man-Userbot Berhasil Di Restart**"
         )
-    # Spin a new instance of bot
     args = [sys.executable, "-m", "userbot"]
     execle(sys.executable, *args, environ)
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"readme$"))
-async def reedme(e):
-    await e.edit(
+@man_cmd(pattern="readme$")
+async def reedme(event):
+    await edit_or_reply(
+        event,
         "**Berikut sesuatu untuk kamu baca:**\n"
         "\n✣ [Userbot Repo](https://github.com/mrismanaziz/Man-Userbot/blob/Man-Userbot/README.md)"
         "\n✣ [Video Tutorial](https://youtu.be/tTDaPKsGC1I)"
         "\n✣ [List Variabel Heroku untuk Man-Userbot](https://telegra.ph/List-Variabel-Heroku-untuk-Man-Userbot-09-22)"
         "\n✣ [Setup Guide - Basic](https://mrismanaziz.medium.com/cara-memasang-userbot-telegram-repo-man-userbot-deploy-di-heroku-c56d1f8b5537)"
-        "\n✣ [Setup Guide - LastFM Module](https://telegra.ph/How-to-set-up-LastFM-module-for-Paperplane-userbot-11-02)"
+        "\n✣ [Setup Guide - LastFM Module](https://telegra.ph/How-to-set-up-LastFM-module-for-Paperplane-userbot-11-02)",
     )
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"repeat (.*)"))
-async def repeat(rep):
-    cnt, txt = rep.pattern_match.group(1).split(" ", 1)
+@man_cmd(pattern="repeat (.*)")
+async def repeat(event):
+    cnt, txt = event.pattern_match.group(1).split(" ", 1)
     replyCount = int(cnt)
     toBeRepeated = txt
 
@@ -106,16 +112,16 @@ async def repeat(rep):
     for _ in range(replyCount - 1):
         replyText += toBeRepeated + "\n"
 
-    await rep.edit(replyText)
+    await edit_or_reply(event, replyText)
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"repo$"))
-async def repo_is_here(wannasee):
-    """For .repo command, just returns the repo URL."""
-    await wannasee.edit(
+@man_cmd(pattern="repo$")
+async def repo_is_here(event):
+    xx = await edit_or_reply(event, "`Processing...`")
+    await xx.edit(
         f"**Hey**, __I am using__ 🔥 **Man-Userbot** 🔥\n\n"
         f"      __Thanks For Using me__\n\n"
-        f"✣ **Userbot Version :** `{BOT_VER}@{UPSTREAM_REPO_BRANCH}`\n"
+        f"✣ **Userbot Version :** `{BOT_VER}@{branch}`\n"
         f"✣ **Group Support :** [Sharing Userbot](t.me/sharinguserbot)\n"
         f"✣ **Channel Man :** [Lunatic0de](t.me/Lunatic0de)\n"
         f"✣ **Owner Repo :** [Risman](t.me/mrismanaziz)\n"
@@ -123,15 +129,15 @@ async def repo_is_here(wannasee):
     )
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"string$"))
-async def repo_is_here(wannasee):
-    """For .repo command, just returns the repo URL."""
-    await wannasee.edit(
-        "✥ **GET STRING SESSION TELEGRAM :** [KLIK DISINI](https://repl.it/@mrismanaziz/stringenSession?lite=1&outputonly=1)\n"
+@man_cmd(pattern="string$")
+async def string_is_here(event):
+    await edit_or_reply(
+        event,
+        "✥ **GET STRING SESSION TELEGRAM :** [KLIK DISINI](https://repl.it/@mrismanaziz/stringenSession?lite=1&outputonly=1)\n",
     )
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"raw$"))
+@man_cmd(pattern="raw$")
 async def raw(event):
     the_real_message = None
     reply_to_id = None
@@ -144,7 +150,9 @@ async def raw(event):
         reply_to_id = event.message.id
     with io.BytesIO(str.encode(the_real_message)) as out_file:
         out_file.name = "raw_message_data.txt"
-        await event.edit("`Check the userbot log for the decoded message data !!`")
+        await edit_or_reply(
+            event, "`Check the userbot log for the decoded message data !!`"
+        )
         await event.client.send_file(
             BOTLOG_CHATID,
             out_file,
@@ -155,26 +163,22 @@ async def raw(event):
         )
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"reverse(?: |$)(\d*)"))
+@man_cmd(pattern="reverse(?: |$)(\d*)")
 async def okgoogle(img):
-    """For .reverse command, Google search images and stickers."""
     if os.path.isfile("okgoogle.png"):
         os.remove("okgoogle.png")
-
     message = await img.get_reply_message()
     if message and message.media:
         photo = io.BytesIO()
-        await bot.download_media(message, photo)
+        await img.client.download_media(message, photo)
     else:
-        await img.edit("**Harap Balas ke Gambar**")
-        return
-
+        return await edit_or_reply(img, "**Harap Balas ke Gambar**")
     if photo:
-        await img.edit("`Processing...`")
+        xx = await edit_or_reply(img, "`Processing...`")
         try:
             image = Image.open(photo)
         except OSError:
-            await img.edit("**Gambar tidak di dukung**")
+            await xx.edit("**Gambar tidak di dukung**")
             return
         name = "okgoogle.png"
         image.save(name, "PNG")
@@ -184,27 +188,22 @@ async def okgoogle(img):
         multipart = {"encoded_image": (name, open(name, "rb")), "image_content": ""}
         response = requests.post(searchUrl, files=multipart, allow_redirects=False)
         fetchUrl = response.headers["Location"]
-
         if response != 400:
-            await img.edit(
+            await xx.edit(
                 "`Image successfully uploaded to Google. Maybe.`"
                 "\n`Parsing source now. Maybe.`"
             )
         else:
-            await img.edit("**Google told me to fuck off.**")
-            return
-
+            return await xx.edit("**Google told me to fuck off.**")
         os.remove(name)
         match = await ParseSauce(fetchUrl + "&preferences?hl=en&fg=1#languages")
         guess = match["best_guess"]
         imgspage = match["similar_images"]
-
         if guess and imgspage:
-            await img.edit(f"[{guess}]({fetchUrl})\n\n`Looking for images...`")
+            await xx.edit(f"[{guess}]({fetchUrl})\n\n`Looking for images...`")
         else:
-            await img.edit("**Tidak dapat menemukan apa pun.**")
+            await xx.edit("**Tidak dapat menemukan apa pun.**")
             return
-
         lim = img.pattern_match.group(1) or 3
         images = await scam(match, lim)
         yeet = []
@@ -219,19 +218,15 @@ async def okgoogle(img):
             )
         except TypeError:
             pass
-        await img.edit(
-            f"[{guess}]({fetchUrl})\n\n[Visually similar images]({imgspage})"
+        await xx.edit(
+            f"[{guess}]({fetchUrl})\n\n[Gambar yang mirip secara visual]({imgspage})"
         )
 
 
 async def ParseSauce(googleurl):
-    """Parse/Scrape the HTML code for the info we want."""
-
     source = opener.open(googleurl).read()
     soup = BeautifulSoup(source, "html.parser")
-
     results = {"similar_images": "", "best_guess": ""}
-
     try:
         for similar_image in soup.findAll("input", {"class": "gLFyf"}):
             url = "https://www.google.com/search?tbm=isch&q=" + urllib.parse.quote_plus(
@@ -240,56 +235,46 @@ async def ParseSauce(googleurl):
             results["similar_images"] = url
     except BaseException:
         pass
-
     for best_guess in soup.findAll("div", attrs={"class": "r5a77d"}):
         results["best_guess"] = best_guess.get_text()
-
     return results
 
 
 async def scam(results, lim):
-
     single = opener.open(results["similar_images"]).read()
     decoded = single.decode("utf-8")
-
     imglinks = []
     counter = 0
-
     pattern = r"^,\[\"(.*[.png|.jpg|.jpeg])\",[0-9]+,[0-9]+\]$"
     oboi = re.findall(pattern, decoded, re.I | re.M)
-
     for imglink in oboi:
         counter += 1
         if counter < int(lim):
             imglinks.append(imglink)
         else:
             break
-
     return imglinks
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"send (.*)"))
+@man_cmd(pattern="send (.*)")
 async def send(event):
-    await event.edit("**Berhasil Mengirim pesan ini**")
-
     if not event.is_reply:
-        return await event.edit("**Mohon Balas ke pesan yang ingin dikirim!**")
-
+        return await edit_or_reply(
+            event, "**Mohon Balas ke pesan yang ingin dikirim!**"
+        )
     chat = event.pattern_match.group(1)
+    xx = await edit_or_reply(event, "**Berhasil Mengirim pesan ini**")
     try:
         chat = int(chat)
     except ValueError:
         pass
-
     try:
         chat = await event.client.get_entity(chat)
     except (TypeError, ValueError):
-        return await event.edit("**Link yang diberikan tidak valid!**")
-
+        return await xx.edit("**Link yang diberikan tidak valid!**")
     message = await event.get_reply_message()
-
     await event.client.send_message(entity=chat, message=message)
-    await event.edit(f"**Berhasil Mengirim pesan ini ke** `{chat.title}`")
+    await xx.edit(f"**Berhasil Mengirim pesan ini ke** `{chat.title}`")
 
 
 CMD_HELP.update(
