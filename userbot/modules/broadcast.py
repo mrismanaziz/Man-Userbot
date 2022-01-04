4d5030f6374ca06970953e898b6e4120c25ca456
mrismanaziz <mrismanaziz@gmail.com>
2022-01-04 09:55:25 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/broadcast.py
+++ userbot/modules/broadcast.py
@@ -5,7 +5,7 @@
 
 from asyncio import sleep
 
-from userbot import BOTLOG, BOTLOG_CHATID
+from userbot import BOTLOG_CHATID
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, LOGS, bot
 from userbot.events import man_cmd
