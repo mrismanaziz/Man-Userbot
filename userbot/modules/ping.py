0694282f1cb0d4eb81257eaacc0d9a85b9748edd
IKHSAN GANTENG <97096980+jookalem@users.noreply.github.com>
2022-01-08 11:43:32 +0700

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -66,10 +66,10 @@ async def _(ping):
     duration = (end - start).microseconds / 1000
     user = await bot.get_me()
     await xx.edit(
-        f"**PONG!!🏓**\n"
-        f"✣ **Pinger** - `%sms`\n"
-        f"✣ **Uptime -** `{uptime}` \n"
-        f"**✦҈͜͡Owner :** [{user.first_name}](tg://user?id={user.id})" % (duration)
+        f"**⚡PONG⚡**\n"
+        f"✣ **Sɪɢɴᴀʟ :** - `%sms`\n"
+        f"✣ **Uᴘᴛɪᴍᴇ :** `{uptime}` \n"
+        f"✣ **Oᴡɴᴇʀ :** [{user.first_name}](tg://user?id={user.id})" % (duration)
     )
 
 
