a7d4daec8b0693946544db961f86902b590b6456
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2022-01-13 21:36:30 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -26,18 +26,12 @@ from PIL import Image
 
 from userbot import BOT_VER, BOTLOG_CHATID
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, HEROKU_API_KEY, HEROKU_APP_NAME, SUDO_USERS
+from userbot import CMD_HELP, HEROKU_APP, HEROKU_API_KEY, HEROKU_APP_NAME, SUDO_USERS
 from userbot.utils import edit_or_reply, man_cmd, time_formatter
-
-# ================= CONSTANT =================
-HEROKU_APP = from_key(HEROKU_API_KEY).apps()[HEROKU_APP_NAME]
-# ============================================
 
 opener = urllib.request.build_opener()
 useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.70 Mobile Safari/537.36"
 opener.addheaders = [("User-agent", useragent)]
-repo = Repo()
-branch = repo.active_branch.name
 
 
 @man_cmd(pattern="sleep ([0-9]+)$")
