71febb4cbf708e94c420d90ccf2f620fcd5fce22
mrismanaziz <mrismanaziz@gmail.com>
2022-01-24 15:31:57 +0700

    Update session.sh

--- userbot/resources/session/session.sh
+++ userbot/resources/session/session.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 # Ultroid - UserBot
-# Copyright (C) 2021-2022 TeamUltroid
+# Copyright (C) 2022 Man-Userbot
 #
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+# This file is a part of < https://github.com/mrismanaziz/Man-Userbot/ >
+# PLease read the GNU Affero General Public License in <https://www.github.com/mrismanaziz/Man-Userbot/blob/main/LICENSE/>
 
 clear
-sec=5
+sec=3
 spinner=(⣻ ⢿ ⡿ ⣟ ⣯ ⣷)
 while [ $sec -gt 0 ]; do
     echo -ne "\e[33m ${spinner[sec]} Starting dependency installation in $sec seconds...\r"
     sleep 1
     sec=$(($sec - 1))
 done
-echo -e "\e[1;32mInstalling Dependencies ---------------------------\e[0m\n" # Don't Remove Dashes / Fix it
+echo -e "\e[1;32mInstalling Dependencies ---------------------------\e[0m\n"
 apt-get update
 apt-get upgrade -y
 pkg upgrade -y
 pkg install python wget -y
-wget https://raw.githubusercontent.com/TeamUltroid/ultroid/main/resources/session/ssgen.py
-pip uninstall telethon -y && install telethon
+wget https://raw.githubusercontent.com/mrismanaziz/Man-Userbot/Man-Userbot/userbot/resources/session/string_session.py
+pip install telethon
 clear
-python3 ssgen.py
+python3 string_session.py
