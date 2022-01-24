7c0ae416c8ef909d6ccdadb6d4ab82691e1a2671
mrismanaziz <mrismanaziz@gmail.com>
2022-01-24 15:57:59 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -211,7 +211,7 @@ ZIP_DOWNLOAD_DIRECTORY = os.environ.get("ZIP_DOWNLOAD_DIRECTORY", "./zips")
 BITLY_TOKEN = os.environ.get("BITLY_TOKEN", None)
 
 # Bot version
-BOT_VER = os.environ.get("BOT_VER", "2.3.5")
+BOT_VER = os.environ.get("BOT_VER", "2.3.4")
 
 # Default .alive logo
 ALIVE_LOGO = (
