082828434f046cdfc1f81a96343e645661fc4cf3
mrismanaziz <mrismanaziz@gmail.com>
2022-01-24 16:16:27 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -71,11 +71,6 @@ async def deploy(xx, repo, ups_rem, ac_br, txt):
             pass
         ups_rem.fetch(ac_br)
         repo.git.reset("--hard", "FETCH_HEAD")
-        repo.config_writer().set_value("user", "name", "mrismanaziz").release()
-        repo.config_writer().set_value(
-            "user", "email", "mrismanaziz@gmail.com"
-        ).release()
-        repo.git.commit("--amend", "--no-edit")
         heroku_git_url = heroku_app.git_url.replace(
             "https://", "https://api:" + HEROKU_API_KEY + "@"
         )
