6bdfdd2405a2a8c2edc43e9a9dc9e49bf11c67a2
MasterParrel <97712767+MasterParrel@users.noreply.github.com>
2022-01-25 13:38:18 +0700

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -58,17 +58,17 @@ async def _(ping):
     """For .ping command, ping the userbot from any chat."""
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    xx = await edit_or_reply(ping, "**✣**")
-    await xx.edit("**✣✣**")
-    await xx.edit("**✣✣✣**")
-    await xx.edit("**✣✣✣✣**")
+    xx = await edit_or_reply(ping, "**💠**")
+    await xx.edit("**💠💠**")
+    await xx.edit("**💠💠💠**")
+    await xx.edit("**💠💠💠💠**")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     user = await bot.get_me()
     await xx.edit(
-        f"**PONG!!🏓**\n"
-        f"✣ **Pinger** - `%sms`\n"
-        f"✣ **Uptime -** `{uptime}` \n"
+        f"**PONG!!🔥**\n"
+        f"💠 **Pinger** - `%sms`\n"
+        f"💠 **Uptime -** `{uptime}` \n"
         f"**✦҈͜͡Owner :** [{user.first_name}](tg://user?id={user.id})" % (duration)
     )
 
