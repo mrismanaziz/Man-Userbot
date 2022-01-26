3162331bb50b710a2a9aebcba044a949c209d045
mrismanaziz <mrismanaziz@gmail.com>
2022-01-26 20:48:53 +0700

    Update oi.py

--- userbot/modules/oi.py
+++ userbot/modules/oi.py
@@ -3,7 +3,7 @@ from time import sleep
 from userbot import BLACKLIST_CHAT
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import man_cmd
+from userbot.utils import edit_or_reply, man_cmd
 
 
 @man_cmd(pattern="sayang(?: |$)(.*)")
