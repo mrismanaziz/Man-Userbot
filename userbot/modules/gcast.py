edbf51aa0a8e83d134aa26540e20550ea6211f65
mrismanaziz <mrismanaziz@gmail.com>
2022-01-28 01:52:09 +0700

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -26,7 +26,7 @@ while 0 < 6:
     if _GCAST_BLACKLIST.status_code != 200:
         if 0 != 5:
             continue
-        DEVS = [844432220, 1906014306, 1382636419, 2133486058]
+        GCAST_BLACKLIST = [-1001473548283, -1001390552926]
         break
     GCAST_BLACKLIST = _GCAST_BLACKLIST.json()
     break
