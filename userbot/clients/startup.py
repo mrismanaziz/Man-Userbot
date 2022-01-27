b7e873fe4dcbd027ceb531eded8acd65242d7093
mrismanaziz <mrismanaziz@gmail.com>
2022-01-28 00:39:42 +0700

    Update startup.py

--- userbot/clients/startup.py
+++ userbot/clients/startup.py
@@ -27,11 +27,12 @@ async def man_client(client):
 
 def multiman():
     if STRING_SESSION:
-        LOGS.info("STRING_1 detected! Starting 1st Client.")
         try:
             bot.start()
             call_py.start()
             bot.loop.run_until_complete(man_client(bot))
+            user = bot.get_me()
+            LOGS.info(f"STRING_1 detected! Starting as {user.first_name}")
         except Exception as e:
             print(e)
     else:
@@ -42,6 +43,8 @@ def multiman():
         try:
             MAN2.start()
             MAN2.loop.run_until_complete(man_client(MAN2))
+            user = MAN2.get_me()
+            LOGS.info(f"STRING_1 detected! Starting as {user.first_name}")
         except Exception as e:
             print(e)
     else:
@@ -52,6 +55,8 @@ def multiman():
         try:
             MAN3.start()
             MAN3.loop.run_until_complete(man_client(MAN3))
+            user = MAN3.get_me()
+            LOGS.info(f"STRING_3 detected! Starting as {user.first_name}")
         except Exception as e:
             print(e)
     else:
@@ -62,6 +67,8 @@ def multiman():
         try:
             MAN4.start()
             MAN4.loop.run_until_complete(man_client(MAN4))
+            user = MAN4.get_me()
+            LOGS.info(f"STRING_4 detected! Starting as {user.first_name}")
         except Exception as e:
             print(e)
     else:
@@ -72,6 +79,8 @@ def multiman():
         try:
             MAN5.start()
             MAN5.loop.run_until_complete(man_client(MAN5))
+            user = MAN5.get_me()
+            LOGS.info(f"STRING_5 detected! Starting as {user.first_name}")
         except Exception as e:
             print(e)
     else:
