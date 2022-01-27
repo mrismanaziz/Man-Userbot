e67d7843edac07c0d0c482c321ad914b01ad956f
mrismanaziz <mrismanaziz@gmail.com>
2022-01-27 10:49:22 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/emojigames.py
+++ userbot/modules/emojigames.py
@@ -6,7 +6,7 @@
 from telethon.tl.types import InputMediaDice
 
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, bot
+from userbot import CMD_HELP
 from userbot.utils import man_cmd
 
 
