94a0da7d4b7693b3f5c15c759dfbf679a2400320
mrismanaziz <mrismanaziz@gmail.com>
2022-01-27 19:15:26 +0700

    Update deepfry.py

--- userbot/modules/deepfry.py
+++ userbot/modules/deepfry.py
@@ -38,11 +38,10 @@ from PIL import Image, ImageEnhance, ImageOps
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, bot
-from userbot.events import man_cmd
-from userbot.utils import check_media
+from userbot.utils import edit_delete, edit_or_reply, check_media, man_cmd
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"deepfry(?: |$)(.*)"))
+@man_cmd(pattern="deepfry(?: |$)(.*)")
 async def deepfryer(event):
     try:
         frycount = int(event.pattern_match.group(1))
@@ -50,35 +49,28 @@ async def deepfryer(event):
             raise ValueError
     except ValueError:
         frycount = 1
-
     if event.is_reply:
         reply_message = await event.get_reply_message()
         data = await check_media(reply_message)
-
         if isinstance(data, bool):
-            await event.edit("`I can't deep fry that!`")
-            return
+            return await edit_delete(event, "`I can't deep fry that!`")
     else:
-        await event.edit("`Reply to an image or sticker to deep fry it!`")
-        return
-
+        return await edit_delete(event, "`Reply to an image or sticker to deep fry it!`")
     # download last photo (highres) as byte array
-    await event.edit("`Downloading media…`")
+    xx = await edit_or_reply(event, "`Downloading media…`")
     image = io.BytesIO()
     await event.client.download_media(data, image)
     image = Image.open(image)
-
     # fry the image
-    await event.edit("`Deep frying media…`")
+    await xx.edit("`Deep frying media…`")
     for _ in range(frycount):
         image = await deepfry(image)
-
     fried_io = io.BytesIO()
     fried_io.name = "image.jpeg"
     image.save(fried_io, "JPEG")
     fried_io.seek(0)
-
-    await event.reply(file=fried_io)
+    await xx.delete()
+    await event.send_file(file=fried_io, reply_to=event.reply_to_msg_id)
 
 
 async def deepfry(img: Image) -> Image:
