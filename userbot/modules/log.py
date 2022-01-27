dd2ab6b5b58a760bb185316f7b19644fb4f98d43
mrismanaziz <mrismanaziz@gmail.com>
2022-01-27 06:44:20 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/log.py
+++ userbot/modules/log.py
@@ -9,7 +9,7 @@ from telethon import events
 
 from userbot import BOTLOG_CHATID
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, LOGS, bot, tgbot
+from userbot import CMD_HELP, LOGS, bot
 from userbot.modules.sql_helper import no_log_pms_sql
 from userbot.modules.sql_helper.globals import addgvar, gvarstatus
 from userbot.modules.vcplugin import vcmention
