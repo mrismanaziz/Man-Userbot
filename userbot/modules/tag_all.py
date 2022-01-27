fad169ff14121a7f934b0c2ee59a3b717b34fdc9
mrismanaziz <mrismanaziz@gmail.com>
2022-01-27 13:39:47 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/tag_all.py
+++ userbot/modules/tag_all.py
@@ -35,7 +35,9 @@ async def _(event):
     chat = await event.get_input_chat()
     async for x in event.client.iter_participants(chat, 100500):
         mentions += f"[\u2063](tg://user?id={x.id} {query})"
-    await event.client.send_message(chat, mentions, reply_to=event.message.reply_to_msg_id)
+    await event.client.send_message(
+        chat, mentions, reply_to=event.message.reply_to_msg_id
+    )
 
 
 @man_cmd(pattern="emojitag(?: |$)(.*)")
