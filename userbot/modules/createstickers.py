92ef47d6c0e308c7dbe4330c517fc78c0338b587
mrismanaziz <mrismanaziz@gmail.com>
2022-01-27 19:33:08 +0700

    Update createstickers.py

--- userbot/modules/createstickers.py
+++ userbot/modules/createstickers.py
@@ -12,54 +12,43 @@ from PIL import Image, ImageDraw, ImageFont
 from telethon.tl.types import InputMessagesFilterDocument
 
 from userbot import bot
-from userbot.events import man_cmd
+from userbot.utils import man_cmd
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"cs(?: |$)(.*)"))
+@man_cmd(pattern=r"cs(?: |$)(.*)")
 async def sticklet(event):
     R = random.randint(0, 256)
     G = random.randint(0, 256)
     B = random.randint(0, 256)
-
     # get the input text
     # the text on which we would like to do the magic on
     sticktext = event.pattern_match.group(1)
-
     # delete the userbot command,
     # i don't know why this is required
     await event.delete()
-
     # https://docs.python.org/3/library/textwrap.html#textwrap.wrap
     sticktext = textwrap.wrap(sticktext, width=10)
     # converts back the list to a string
     sticktext = "\n".join(sticktext)
-
     image = Image.new("RGBA", (512, 512), (255, 255, 255, 0))
     draw = ImageDraw.Draw(image)
     fontsize = 230
-
     FONT_FILE = await get_font_file(event.client, "@xcruzfont")
-
     font = ImageFont.truetype(FONT_FILE, size=fontsize)
-
     while draw.multiline_textsize(sticktext, font=font) > (512, 512):
         fontsize -= 3
         font = ImageFont.truetype(FONT_FILE, size=fontsize)
-
     width, height = draw.multiline_textsize(sticktext, font=font)
     draw.multiline_text(
         ((512 - width) / 2, (512 - height) / 2), sticktext, font=font, fill=(R, G, B)
     )
-
     image_stream = io.BytesIO()
     image_stream.name = "@sticker.webp"
     image.save(image_stream, "WebP")
     image_stream.seek(0)
-
     # finally, reply the sticker
     # await event.reply( file=image_stream, reply_to=event.message.reply_to_msg_id)
     # replacing upper line with this to get reply tags
-
     await event.client.send_file(
         event.chat_id, image_stream, reply_to=event.message.reply_to_msg_id
     )
