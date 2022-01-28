f0d5c40d714afc1614573715e6918ea49aa8a418
mrismanaziz <mrismanaziz@gmail.com>
2022-01-28 21:40:15 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/appmisc.py
+++ userbot/modules/appmisc.py
@@ -26,7 +26,7 @@ from telethon.tl.types import (
 )
 
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, SUDO_USERS, TEMP_DOWNLOAD_DIRECTORY, bot
+from userbot import CMD_HELP, SUDO_USERS, TEMP_DOWNLOAD_DIRECTORY
 from userbot.utils import edit_delete, edit_or_reply, man_cmd
 
 normiefont = [
