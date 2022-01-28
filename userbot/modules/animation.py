6a1e7d6ba607daa1cf0a8ca3839dad6d97071b6b
mrismanaziz <mrismanaziz@gmail.com>
2022-01-28 21:58:32 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/animation.py
+++ userbot/modules/animation.py
@@ -2,7 +2,7 @@ import asyncio
 from time import sleep
 
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, bot
+from userbot import CMD_HELP
 from userbot.utils import edit_or_reply, man_cmd
 
 
