a04b1fb45fdd52eb04d953e3423df9e64fbf1f04
mrismanaziz <mrismanaziz@gmail.com>
2022-01-29 15:24:34 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -27,7 +27,8 @@ async def _(event):
 
 @man_cmd(pattern="P(?: |$)(.*)")
 async def _(event):
-    xx = await edit_or_reply(event, f"**Haii Salken Saya {owner}**")
+    me = event.client.get_me()
+    xx = await edit_or_reply(event, f"**Haii Salken Saya {me.first_name}**")
     sleep(2)
     await xx.edit("**Assalamualaikum...**")
 
@@ -42,7 +43,8 @@ async def _(event):
 
 @man_cmd(pattern="a(?: |$)(.*)")
 async def _(event):
-    xx = await edit_or_reply(event, f"**Haii Salken Saya {owner}**")
+    me = event.client.get_me()
+    xx = await edit_or_reply(event, f"**Haii Salken Saya {me.first_name}**")
     sleep(2)
     await xx.edit("**Assalamualaikum**")
 
@@ -56,7 +58,8 @@ async def _(event):
 
 @man_cmd(pattern="k(?: |$)(.*)")
 async def _(event):
-    xx = await edit_or_reply(event, f"**Hallo KIMAAKK SAYA {owner}**")
+    me = event.client.get_me()
+    xx = await edit_or_reply(event, f"**Hallo KIMAAKK SAYA {me.first_name}**")
     sleep(2)
     await xx.edit("**LU SEMUA NGENTOT 🔥**")
 
