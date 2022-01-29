dcd69e0a1e09465323a859473cb7585d1865c325
mrismanaziz <mrismanaziz@gmail.com>
2022-01-29 08:38:01 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -35,7 +35,7 @@ from userbot import BOT_USERNAME
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
 from userbot import S_PACK_NAME as custompack
-from userbot import tgbot, user
+from userbot import tgbot
 from userbot.modules.sql_helper.globals import addgvar, gvarstatus
 from userbot.utils import edit_delete, edit_or_reply, man_cmd
 
