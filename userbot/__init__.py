afe486a2eb5cf377a9554115a88a7242895aa81a
mrismanaziz <mrismanaziz@gmail.com>
2022-01-30 06:01:20 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -106,6 +106,10 @@ API_HASH = str(os.environ.get("API_HASH") or None)
 
 # Userbot Session String
 STRING_SESSION = os.environ.get("STRING_SESSION", None)
+try:
+    STRING_2 = os.environ.get("STRING_2", None)
+except ValueError:
+    raise Exception("STRING_2 Yang anda Masukan Tidak Valid. Silahkan Buat STRING Telethon Baru.")
 STRING_2 = os.environ.get("STRING_2", None)
 STRING_3 = os.environ.get("STRING_3", None)
 STRING_4 = os.environ.get("STRING_4", None)
