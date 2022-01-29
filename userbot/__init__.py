2135fe8c36c7ad053f51e82612802bdd4b18680d
mrismanaziz <mrismanaziz@gmail.com>
2022-01-30 02:50:34 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -537,7 +537,7 @@ with bot:
             idme = user.id
             ids = await client_id(event, event.query.user_id)
             OWNER_ID, MAN_USER, man_mention = ids[0], ids[1], ids[2]
-            if event.query.user_id == idme and query.startswith("@SharingUserbot"):
+            if event.query.user_id and query.startswith("@SharingUserbot"):
                 buttons = paginate_help(0, dugmeler, "helpme")
                 result = builder.photo(
                     file=logoman,
