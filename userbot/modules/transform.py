1c0db82ba6ec7d484b5df05df055506b258698ae
mrismanaziz <mrismanaziz@gmail.com>
2022-01-29 23:45:16 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/transform.py
+++ userbot/modules/transform.py
@@ -11,7 +11,7 @@ from telethon.tl.types import DocumentAttributeFilename
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
 from userbot.events import register
-from userbot.utils import bash, edit_delete, edit_or_reply, man_cmd
+from userbot.utils import edit_delete, edit_or_reply, man_cmd
 
 Converted = TEMP_DOWNLOAD_DIRECTORY + "sticker.webp"
 
