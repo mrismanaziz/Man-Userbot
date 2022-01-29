d2921e300cff80cdf9cbf8014788e36ba2f79bd5
mrismanaziz <mrismanaziz@gmail.com>
2022-01-29 08:29:35 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/vctools.py
+++ userbot/modules/vctools.py
@@ -18,7 +18,7 @@ from telethon.tl.functions.phone import GetGroupCallRequest as getvc
 from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
 
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, owner
+from userbot import CMD_HELP
 from userbot.events import register
 from userbot.utils import edit_delete, edit_or_reply, man_cmd
 
