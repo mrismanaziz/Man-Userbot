afb15821e4ede1250bbddeb2da4cde5604934a3d
mrismanaziz <mrismanaziz@gmail.com>
2022-01-29 08:27:40 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/helper.py
+++ userbot/modules/helper.py
@@ -1,6 +1,6 @@
 """ Userbot module for other small commands. """
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, owner
+from userbot import CMD_HELP
 from userbot.utils import edit_or_reply, man_cmd
 
 
