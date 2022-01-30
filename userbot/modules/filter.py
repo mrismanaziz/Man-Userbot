8edb4d291f410fbcaab7b428a1f626201764d397
mrismanaziz <mrismanaziz@gmail.com>
2022-01-30 11:30:30 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/filter.py
+++ userbot/modules/filter.py
@@ -8,14 +8,18 @@
 import asyncio
 import re
 
-from telethon import events, utils as ut
+from telethon import utils as ut
 from telethon.tl import types
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.modules.sql_helper.filter_sql import get_filter, add_filter, remove_filter, get_all_filters, remove_all_filters
-from userbot.utils import eod, oer, man_cmd
-
+from userbot.modules.sql_helper.filter_sql import (
+    add_filter,
+    get_all_filters,
+    remove_all_filters,
+    remove_filter,
+)
+from userbot.utils import eod, man_cmd
 
 DELETE_TIMEOUT = 0
 TYPE_TEXT = 0
@@ -25,6 +29,7 @@ TYPE_DOCUMENT = 2
 
 global last_triggered_filters
 last_triggered_filters = {}
+
 
 @man_cmd(incoming=True)
 async def on_snip(event):
@@ -42,23 +47,20 @@ async def on_snip(event):
                     media = types.InputPhoto(
                         int(snip.media_id),
                         int(snip.media_access_hash),
-                        snip.media_file_reference
+                        snip.media_file_reference,
                     )
                 elif snip.snip_type == TYPE_DOCUMENT:
                     media = types.InputDocument(
                         int(snip.media_id),
                         int(snip.media_access_hash),
-                        snip.media_file_reference
+                        snip.media_file_reference,
                     )
                 else:
                     media = None
-                message_id = event.message.id
+                event.message.id
                 if event.reply_to_msg_id:
-                    message_id = event.reply_to_msg_id
-                await event.reply(
-                    snip.reply,
-                    file=media
-                )
+                    event.reply_to_msg_id
+                await event.reply(snip.reply, file=media)
                 if event.chat_id not in last_triggered_filters:
                     last_triggered_filters[event.chat_id] = []
                 last_triggered_filters[event.chat_id].append(name)
@@ -71,23 +73,33 @@ async def on_snip_save(event):
     name = event.pattern_match.group(1)
     msg = await event.get_reply_message()
     if msg:
-        snip = {'type': TYPE_TEXT, 'text': msg.message or ''}
+        snip = {"type": TYPE_TEXT, "text": msg.message or ""}
         if msg.media:
             media = None
             if isinstance(msg.media, types.MessageMediaPhoto):
                 media = ut.get_input_photo(msg.media.photo)
-                snip['type'] = TYPE_PHOTO
+                snip["type"] = TYPE_PHOTO
             elif isinstance(msg.media, types.MessageMediaDocument):
                 media = ut.get_input_document(msg.media.document)
-                snip['type'] = TYPE_DOCUMENT
+                snip["type"] = TYPE_DOCUMENT
             if media:
-                snip['id'] = media.id
-                snip['hash'] = media.access_hash
-                snip['fr'] = media.file_reference
-        add_filter(event.chat_id, name, snip['text'], snip['type'], snip.get('id'), snip.get('hash'), snip.get('fr'))
+                snip["id"] = media.id
+                snip["hash"] = media.access_hash
+                snip["fr"] = media.file_reference
+        add_filter(
+            event.chat_id,
+            name,
+            snip["text"],
+            snip["type"],
+            snip.get("id"),
+            snip.get("hash"),
+            snip.get("fr"),
+        )
         await eod(event, f"**Berhasil Menambahkan Filter** `{name}")
     else:
-        await eod(event, f"Balas pesan dengan `{cmd}filter keyword` untuk menyimpan filter")
+        await eod(
+            event, f"Balas pesan dengan `{cmd}filter keyword` untuk menyimpan filter"
+        )
 
 
 @man_cmd(pattern="filters$")
@@ -108,7 +120,7 @@ async def on_snip_list(event):
                 force_document=True,
                 allow_cache=False,
                 caption="Daftar Filter Yang Aktif Disini",
-                reply_to=event
+                reply_to=event,
             )
             await event.delete()
     else:
