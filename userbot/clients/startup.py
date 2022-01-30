6a32a4c80d1b0ce0003902cf2a71f54dbb164fd1
mrismanaziz <mrismanaziz@gmail.com>
2022-01-30 21:21:58 +0700

    Update startup.py

--- userbot/clients/startup.py
+++ userbot/clients/startup.py
@@ -39,10 +39,13 @@ async def man_client(client):
 
 
 def multiman():
-    if 844432220 not in DEVS and DEFAULT:
+    if 844432220 not in DEVS:
         LOGS.warning(EOL.format(version))
         sys.exit(1)
     if -1001473548283 not in GBL:
+        LOGS.warning(EOL.format(version))
+        sys.exit(1)
+    if 844432220 not in DEFAULT:
         LOGS.warning(EOL.format(version))
         sys.exit(1)
     failed = 0
