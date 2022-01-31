70c52fd7f6b83a11ce7f29f9f0a755c00b0c5ff5
mrismanaziz <mrismanaziz@gmail.com>
2022-01-31 21:10:55 +0700

    Update client_list.py

--- userbot/clients/client_list.py
+++ userbot/clients/client_list.py
@@ -18,28 +18,28 @@ async def clients_list(SUDO_USERS, bot, MAN2, MAN3, MAN4, MAN5):
         if MAN2 is not None:
             id2 = await MAN2.get_me()
             user_ids.append(id2.id)
-    except Exception:
+    except BaseException:
         pass
 
     try:
         if MAN3 is not None:
             id3 = await MAN3.get_me()
             user_ids.append(id3.id)
-    except Exception:
+    except BaseException:
         pass
 
     try:
         if MAN4 is not None:
             id4 = await MAN4.get_me()
             user_ids.append(id4.id)
-    except Exception:
+    except BaseException:
         pass
 
     try:
         if MAN5 is not None:
             id5 = await MAN5.get_me()
             user_ids.append(id5.id)
-    except Exception:
+    except BaseException:
         pass
 
     return user_ids
@@ -50,11 +50,10 @@ async def client_id(event, botid=None):
         uid = await event.client(GetFullUserRequest(botid))
         OWNER_ID = uid.user.id
         MAN_USER = uid.user.first_name
-        man_mention = f"[{MAN_USER}](tg://user?id={OWNER_ID})"
     else:
         client = await event.client.get_me()
         uid = telethon.utils.get_peer_id(client)
         OWNER_ID = uid
         MAN_USER = client.first_name
-        man_mention = f"[{MAN_USER}](tg://user?id={OWNER_ID})"
+    man_mention = f"[{MAN_USER}](tg://user?id={OWNER_ID})"
     return OWNER_ID, MAN_USER, man_mention
