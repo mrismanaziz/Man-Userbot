6c1b057fa449b3d40154bdc79a5001c016488d7c
mrismanaziz <mrismanaziz@gmail.com>
2022-02-21 12:46:58 +0700

    Update carbon.py

--- userbot/modules/carbon.py
+++ userbot/modules/carbon.py
@@ -12,11 +12,10 @@
 import os
 import random
 
-from carbonnow import Carbon
-
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
 from userbot.utils import edit_delete, edit_or_reply, man_cmd
+from userbot.utils.misc import Carbon
 
 from .vcplugin import vcmention
 
@@ -177,7 +176,7 @@ async def crbn(event):
     from_user = vcmention(event.sender)
     xxxx = await edit_or_reply(event, "`Processing...`")
     te = event.text
-    col = random.choice(all_col) if te[1] == "r" else None
+    col = random.choice(all_col) if te[1] == "r" else "White"
     if event.reply_to_msg_id:
         temp = await event.get_reply_message()
         if temp.media:
@@ -194,10 +193,7 @@ async def crbn(event):
             return await edit_delete(
                 xxxx, "**Balas ke pesan atau file yang dapat dibaca**", 30
             )
-    carbon = Carbon(
-        base_url="https://carbonara.vercel.app/api/cook", code=code, background=col
-    )
-    xx = await carbon.memorize("carbon_man")
+    xx = await Carbon(code=code, file_name="carbon_man", backgroundColor=col)
     await xxxx.delete()
     await event.reply(
         f"**Carbonised by** {from_user}",
@@ -208,7 +204,7 @@ async def crbn(event):
 @man_cmd(pattern="ccarbon ?(.*)")
 async def crbn(event):
     from_user = vcmention(event.sender)
-    match = event.pattern_match.group(1)
+    match = event.pattern_match.group(1).strip()
     if not match:
         return await edit_or_reply(
             event, "**Berikan Warna Custom untuk Membuat Carbon**"
@@ -232,13 +228,7 @@ async def crbn(event):
             return await edit_delete(
                 msg, "**Balas pesan atau file yang dapat dibaca**", 30
             )
-    carbon = Carbon(
-        base_url="https://carbonara.vercel.app/api/cook", code=code, background=match
-    )
-    try:
-        xx = await carbon.memorize("carbon_man")
-    except Exception as er:
-        return await msg.edit(str(er))
+    xx = await Carbon(code=code, backgroundColor=match)
     await msg.delete()
     await event.reply(
         f"**Carbonised by** {from_user}",
