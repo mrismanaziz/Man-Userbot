d070f1deb19f440930a2cf440134a45849babc33
mrismanaziz <mrismanaziz@gmail.com>
2022-02-21 11:04:11 +0700

    Update misc.py

--- userbot/utils/misc.py
+++ userbot/utils/misc.py
@@ -3,7 +3,7 @@
 #
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+# <https://www.github.com/TeamUltroid/pyUltroid/blob/main/LICENSE/>.
 #
 # Ported by @mrismanaziz
 # FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
