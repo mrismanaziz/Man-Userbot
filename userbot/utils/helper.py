e8d886b3c4f70f4b29df22655dd8ff755fe9753c
mrismanaziz <mrismanaziz@gmail.com>
2022-02-25 04:35:20 +0700

    Create helper.py

---
+++ userbot/utils/helper.py
@@ -0,0 +1,83 @@
+# Ultroid - UserBot
+# Copyright (C) 2021-2022 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
+
+import asyncio
+import multiprocessing
+import sys
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial, wraps
+
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
+from userbot import BOT_VER as manubot_version
+from userbot import LOGS
+from userbot.utils import bash
+
+
+def run_async(function):
+    @wraps(function)
+    async def wrapper(*args, **kwargs):
+        return await asyncio.get_event_loop().run_in_executor(
+            ThreadPoolExecutor(max_workers=multiprocessing.cpu_count() * 5),
+            partial(function, *args, **kwargs),
+        )
+
+    return wrapper
+
+
+async def updateme_requirements():
+    await bash(f"{sys.executable} -m pip install --no-cache-dir -r requirements.txt")
+
+
+@run_async
+def gen_chlog(repo, diff):
+    UPSTREAM_REPO_URL = Repo().remotes[0].config_reader.get("url").replace(".git", "")
+    ac_br = repo.active_branch.name
+    ch_log = tldr_log = ""
+    ch = f"<b>Man-Userbot {manubot_version} updates for <a href={UPSTREAM_REPO_URL}/tree/{ac_br}>[{ac_br}]</a>:</b>"
+    ch_tl = f"Man-Userbot {manubot_version} updates for {ac_br}:"
+    d_form = "%d/%m/%y || %H:%M"
+    for c in repo.iter_commits(diff):
+        ch_log += f"\n\n💬 <b>{c.count()}</b> 🗓 <b>[{c.committed_datetime.strftime(d_form)}]</b>\n<b><a href={UPSTREAM_REPO_URL.rstrip('/')}/commit/{c}>[{c.summary}]</a></b> 👨‍💻 <code>{c.author}</code>"
+        tldr_log += f"\n\n💬 {c.count()} 🗓 [{c.committed_datetime.strftime(d_form)}]\n[{c.summary}] 👨‍💻 {c.author}"
+    if ch_log:
+        return str(ch + ch_log), str(ch_tl + tldr_log)
+    return ch_log, tldr_log
+
+
+async def updater():
+    try:
+        off_repo = Repo().remotes[0].config_reader.get("url").replace(".git", "")
+    except Exception as er:
+        LOGS.exception(er)
+        return
+    try:
+        repo = Repo()
+    except NoSuchPathError as error:
+        LOGS.info(f"`Directory {error} is not found`")
+        Repo().__del__()
+        return
+    except GitCommandError as error:
+        LOGS.info(f"`Early failure! {error}`")
+        Repo().__del__()
+        return
+    except InvalidGitRepositoryError:
+        repo = Repo.init()
+        origin = repo.create_remote("upstream", off_repo)
+        origin.fetch()
+        repo.create_head("Man-Userbot", origin.refs.main)
+        repo.heads.main.set_tracking_branch(origin.refs.main)
+        repo.heads.main.checkout(True)
+    ac_br = repo.active_branch.name
+    try:
+        repo.create_remote("upstream", off_repo)
+    except Exception as er:
+        LOGS.info(er)
+    ups_rem = repo.remote("upstream")
+    ups_rem.fetch(ac_br)
+    changelog, tl_chnglog = await gen_chlog(repo, f"HEAD..upstream/{ac_br}")
+    return bool(changelog)
