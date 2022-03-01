cea8746434826f41f00a605383267e792a873079
mrismanaziz <mrismanaziz@gmail.com>
2022-03-01 08:44:30 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -10,7 +10,7 @@ from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, HEROKU_API_KEY, HEROKU_APP_NAME, UPSTREAM_REPO_URL
+from userbot import CMD_HELP, HEROKU_API_KEY, HEROKU_APP_NAME
 from userbot.events import register
 from userbot.utils import edit_delete, edit_or_reply, man_cmd
 
@@ -129,7 +129,9 @@ async def upstream(event):
     "For .update command, check if the bot is up to date, update if specified"
     xx = await edit_or_reply(event, "`Mengecek Pembaruan, Tunggu Sebentar...`")
     conf = event.pattern_match.group(1).strip()
-    off_repo = b64decode("aHR0cHM6Ly9naXRodWIuY29tL21yaXNtYW5heml6L01hbi1Vc2VyYm90").decode("utf-8")
+    off_repo = b64decode(
+        "aHR0cHM6Ly9naXRodWIuY29tL21yaXNtYW5heml6L01hbi1Vc2VyYm90"
+    ).decode("utf-8")
     force_update = False
     try:
         txt = (
