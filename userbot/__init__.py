63ad5480eecfe8e6f86fe7f63689fa8f03fa1819
mrismanaziz <mrismanaziz@gmail.com>
2022-03-17 08:53:24 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -35,6 +35,7 @@ from telethon.sessions import StringSession
 from telethon.sync import TelegramClient, custom, events
 from telethon.tl.types import InputWebDocument
 from telethon.utils import get_display_name
+
 from userbot.core.git import git
 
 from .storage import Storage
