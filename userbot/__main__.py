9944b7f177efa63b86f256d4bf606fa0b619ad84
mrismanaziz <mrismanaziz@gmail.com>
2022-03-17 12:02:39 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -19,10 +19,12 @@ from pytgcalls import __version__ as pytgcalls
 from pytgcalls import idle
 from telethon import version
 
+from userbot import BOT_TOKEN
 from userbot import BOT_VER as ubotversion
 from userbot import LOGS, bot
 from userbot.clients import man_userbot_on, multiman
 from userbot.modules import ALL_MODULES
+from userbot.utils import autobot
 
 try:
     for module_name in ALL_MODULES:
@@ -42,6 +44,8 @@ except BaseException as e:
 
 
 bot.loop.run_until_complete(man_userbot_on())
+if not BOT_TOKEN:
+    bot.loop.run_until_complete(autobot())
 idle()
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
