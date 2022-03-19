f4c65b0b82d29052718df9a59a5a20602fef85f6
mrismanaziz <mrismanaziz@gmail.com>
2022-03-20 05:43:33 +0700

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -57,7 +57,7 @@ async def gcast(event):
     async for x in event.client.iter_dialogs():
         if x.is_group:
             chat = x.id
-            if chat not in GCAST_BLACKLIST and not BLACKLIST_GCAST:
+            if not int("-100" + str(chat)) in GCAST_BLACKLIST and BLACKLIST_GCAST:
                 try:
                     await event.client.send_message(chat, msg)
                     await asyncio.sleep(0.1)
