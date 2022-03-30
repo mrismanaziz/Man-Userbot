908545371859ba1e841927894e03f5b5c63d82cf
mrismanaziz <mrismanaziz@gmail.com>
2022-03-30 07:49:17 +0700

    Update __init__.py

--- userbot/core/vcbot/__init__.py
+++ userbot/core/vcbot/__init__.py
@@ -1,3 +1,5 @@
 from .chattitle import CHAT_TITLE
-from .queues import add_to_queue, clear_queue, get_queue, pop_an_item
+from .controls import skip_current_song, skip_item
+from .queues import QUEUE, add_to_queue, clear_queue, get_queue, pop_an_item
 from .thumbnail import gen_thumb
+from .ytdl import ytsearch, ytdl
