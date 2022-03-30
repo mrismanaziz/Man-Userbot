7f4dbf998c44682d4824fdd9567f58d1a3134c05
mrismanaziz <mrismanaziz@gmail.com>
2022-03-30 00:49:50 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/core/vcbot/__init__.py
+++ userbot/core/vcbot/__init__.py
@@ -2,4 +2,4 @@ from .chattitle import CHAT_TITLE
 from .controls import skip_current_song, skip_item
 from .queues import QUEUE, add_to_queue, clear_queue, get_queue, pop_an_item
 from .thumbnail import gen_thumb
-from .ytdl import ytsearch, ytdl
+from .ytdl import ytdl, ytsearch
