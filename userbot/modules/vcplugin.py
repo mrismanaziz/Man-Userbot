cb4c30662ce4d02b86d6c961f5c63c16ec484df9
mrismanaziz <mrismanaziz@gmail.com>
2022-03-30 08:55:15 +0700

    Update vcplugin.py

--- userbot/modules/vcplugin.py
+++ userbot/modules/vcplugin.py
@@ -40,7 +40,7 @@ def vcmention(user):
     return f"[{full_name}](tg://user?id={user.id})"
 
 
-@man_cmd(pattern="play(?:\s|$)([\s\S]*)", func=lambda x: not x.is_private)
+@man_cmd(pattern="play(?:\s|$)([\s\S]*)")
 async def vc_play(event):
     title = event.pattern_match.group(1)
     replied = await event.get_reply_message()
@@ -153,7 +153,7 @@ async def vc_play(event):
                 await botman.edit(f"`{ep}`")
 
 
-@man_cmd(pattern="vplay(?:\s|$)([\s\S]*)", func=lambda x: not x.is_private)
+@man_cmd(pattern="vplay(?:\s|$)([\s\S]*)")
 async def vc_vplay(event):
     title = event.pattern_match.group(1)
     replied = await event.get_reply_message()
@@ -330,7 +330,7 @@ async def vc_vplay(event):
                     await xnxx.edit(f"`{ep}`")
 
 
-@man_cmd(pattern="end$", func=lambda x: not x.is_private)
+@man_cmd(pattern="end$")
 async def vc_end(event):
     chat_id = event.chat_id
     if chat_id in QUEUE:
@@ -344,7 +344,7 @@ async def vc_end(event):
         await edit_delete(event, "**Tidak Sedang Memutar Streaming**")
 
 
-@man_cmd(pattern="skip(?:\s|$)([\s\S]*)", func=lambda x: not x.is_private)
+@man_cmd(pattern="skip(?:\s|$)([\s\S]*)")
 async def vc_skip(event):
     chat_id = event.chat_id
     if len(event.text.split()) < 2:
@@ -373,7 +373,7 @@ async def vc_skip(event):
             await event.edit(DELQUE)
 
 
-@man_cmd(pattern="pause$", func=lambda x: not x.is_private)
+@man_cmd(pattern="pause$")
 async def vc_pause(event):
     chat_id = event.chat_id
     if chat_id in QUEUE:
@@ -386,7 +386,7 @@ async def vc_pause(event):
         await edit_delete(event, "**Tidak Sedang Memutar Streaming**")
 
 
-@man_cmd(pattern="resume$", func=lambda x: not x.is_private)
+@man_cmd(pattern="resume$")
 async def vc_resume(event):
     chat_id = event.chat_id
     if chat_id in QUEUE:
@@ -399,7 +399,7 @@ async def vc_resume(event):
         await edit_delete(event, "**Tidak Sedang Memutar Streaming**")
 
 
-@man_cmd(pattern=r"volume(?: |$)(.*)", func=lambda x: not x.is_private)
+@man_cmd(pattern=r"volume(?: |$)(.*)")
 async def vc_volume(event):
     query = event.pattern_match.group(1)
     me = await event.client.get_me()
@@ -422,7 +422,7 @@ async def vc_volume(event):
         await edit_delete(event, "**Tidak Sedang Memutar Streaming**")
 
 
-@man_cmd(pattern="playlist$", func=lambda x: not x.is_private)
+@man_cmd(pattern="playlist$")
 async def vc_playlist(event):
     chat_id = event.chat_id
     if chat_id in QUEUE:
