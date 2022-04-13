eaf739fff67389c11d4d6a33fa62d7c50e66a44b
mrismanaziz <mrismanaziz@gmail.com>
2022-04-13 17:42:40 +0700

    vctools: joinvc: don't leave if it's already joined

--- userbot/modules/vctools.py
+++ userbot/modules/vctools.py
@@ -125,8 +125,8 @@ async def _(event):
             return await Man.edit(f"**ERROR:** `{e}`")
     else:
         chat_id = event.chat_id
-    file = "./userbot/resources/audio-man.mp3"
     if chat_id:
+        file = "./userbot/resources/audio-man.mp3"
         try:
             await call_py.join_group_call(
                 chat_id,
@@ -141,14 +141,11 @@ async def _(event):
                 f"❏ **Berhasil Join Ke Obrolan Suara**\n└ **Chat ID:** `{chat_id}`"
             )
         except AlreadyJoinedError:
-            await call_py.leave_group_call(chat_id)
-            await edit_delete(
-                Man,
-                "**ERROR:** `Karena akun sedang berada di obrolan suara`\n\n• Silahkan coba `.joinvc` lagi",
-                45,
+            return await edit_delete(
+                Man, "**INFO:** `akun anda sudah berada di obrolan suara`", 45
             )
         except Exception as e:
-            await Man.edit(f"**INFO:** `{e}`")
+            return await Man.edit(f"**INFO:** `{e}`")
 
 
 @man_cmd(pattern="leavevc(?: |$)(.*)")
@@ -171,7 +168,7 @@ async def vc_end(event):
                 f"❏ **Berhasil Turun dari Obrolan Suara**\n└ **Chat ID:** `{chat_id}`",
             )
         except Exception as e:
-            await Man.edit(f"**INFO:** `{e}`")
+            return await Man.edit(f"**INFO:** `{e}`")
 
 
 CMD_HELP.update(
